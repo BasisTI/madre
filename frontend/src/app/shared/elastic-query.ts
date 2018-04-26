f078e53403994e317fdf7c2cad9f9d80814f31ee
Jefferson Farias <jefferson.farias@basis.com.br>
2018-04-26 18:00:42 -0300

    BASIS-37210 alteração no css principal

--- frontend/src/app/shared/elastic-query.ts
+++ frontend/src/app/shared/elastic-query.ts
@@ -5,7 +5,7 @@ export class ElasticQuery {
     value: string;
 
     get query(): string {
-        return this.value ? '"*' + this.value + '*"' : ElasticQuery.wildcard;
+        return this.value ? ('"*' + this.value + '*"') : ElasticQuery.wildcard;
     }
 
     reset() {
