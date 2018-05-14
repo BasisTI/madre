f3a64e1672d6d09eca1b009d4deb44ae075d90d4
Lucas Almeida <lucas.almeida@basis.com.br>
2018-05-14 14:31:18 -0300

    BASIS-38140 Sonar

--- cadastros-basicos/src/main/java/br/com/basis/madre/cadastros/config/CacheConfiguration.java
+++ cadastros-basicos/src/main/java/br/com/basis/madre/cadastros/config/CacheConfiguration.java
@@ -82,14 +82,10 @@ public class CacheConfiguration {
         if (this.registration == null) {
             log.warn("No discovery service is set up, Hazelcast cannot create a cluster.");
         } else {
-            // The serviceId is by default the application's name, see Spring Boot's eureka.instance.appname property
             String serviceId = registration.getServiceId();
             log.debug("Configuring Hazelcast clustering for instanceId: {}", serviceId);
-            // In development, everything goes through 127.0.0.1, with a different port
             if (env.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)) {
-                log.debug("Application is running with the \"dev\" profile, Hazelcast " +
-                          "cluster will only work with localhost instances");
-
+                log.debug("Application is running with the \"dev\" profile, Hazelcast " +"cluster will only work with localhost instances");
                 System.setProperty("hazelcast.local.localAddress", "127.0.0.1");
                 config.getNetworkConfig().setPort(serverProperties.getPort() + 5701);
                 config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
@@ -109,8 +105,6 @@ public class CacheConfiguration {
             }
         }
         config.getMapConfigs().put("default", initializeDefaultMapConfig());
-
-        // Full reference is available at: http://docs.hazelcast.org/docs/management-center/3.9/manual/html/Deploying_and_Starting.html
         config.setManagementCenterConfig(initializeDefaultManagementCenterConfig(jHipsterProperties));
         config.getMapConfigs().put("br.com.basis.madre.cadastros.domain.*", initializeDomainMapConfig(jHipsterProperties));
         return Hazelcast.newHazelcastInstance(config);
@@ -127,28 +121,12 @@ public class CacheConfiguration {
     private MapConfig initializeDefaultMapConfig() {
         MapConfig mapConfig = new MapConfig();
 
-    /*
-        Number of backups. If 1 is set as the backup-count for example,
-        then all entries of the map will be copied to another JVM for
-        fail-safety. Valid numbers are 0 (no backup), 1, 2, 3.
-     */
         mapConfig.setBackupCount(0);
 
-    /*
-        Valid values are:
-        NONE (no eviction),
-        LRU (Least Recently Used),
-        LFU (Least Frequently Used).
-        NONE is the default.
-     */
+
         mapConfig.setEvictionPolicy(EvictionPolicy.LRU);
 
-    /*
-        Maximum size of the map. When max size is reached,
-        map is evicted based on the policy defined.
-        Any integer between 0 and Integer.MAX_VALUE. 0 means
-        Integer.MAX_VALUE. Default is 0.
-     */
+
         mapConfig.setMaxSizeConfig(new MaxSizeConfig(0, MaxSizeConfig.MaxSizePolicy.USED_HEAP_SIZE));
 
         return mapConfig;
