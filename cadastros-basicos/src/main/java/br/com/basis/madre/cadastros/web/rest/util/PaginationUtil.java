7bdaaf2e0919d33dfdcde746cb046edd27fd64c6
Brayan Quartezani Costa <brayan.costa@basis.com.br>
2018-05-15 17:01:10 -0300

    BASIS-38152 - correção erros

--- cadastros-basicos/src/main/java/br/com/basis/madre/cadastros/web/rest/util/PaginationUtil.java
+++ cadastros-basicos/src/main/java/br/com/basis/madre/cadastros/web/rest/util/PaginationUtil.java
@@ -1,5 +1,7 @@
 package br.com.basis.madre.cadastros.web.rest.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -11,14 +13,19 @@ import java.net.URLEncoder;
  * Utility class for handling pagination.
  *
  * <p>
- * Pagination uses the same principles as the <a href="https://developer.github.com/v3/#pagination">GitHub API</a>,
- * and follow <a href="http://tools.ietf.org/html/rfc5988">RFC 5988 (Link header)</a>.
+ * Pagination uses the same principles as the
+ * <a href="https://developer.github.com/v3/#pagination">GitHub API</a>, and
+ * follow <a href="http://tools.ietf.org/html/rfc5988">RFC 5988 (Link
+ * header)</a>.
  */
 public final class PaginationUtil {
 
     private PaginationUtil() {
     }
-    private static final  String QUEST = "&query=";
+
+    private static final String QUEST = "&query=";
+    private static final Logger log = LoggerFactory.getLogger(PaginationUtil.class);
+
     public static HttpHeaders generatePaginationHttpHeaders(Page page, String baseUrl) {
 
         HttpHeaders headers = new HttpHeaders();
@@ -43,30 +50,36 @@ public final class PaginationUtil {
     }
 
     private static String generateUri(String baseUrl, int page, int size) {
-        return UriComponentsBuilder.fromUriString(baseUrl).queryParam("page", page).queryParam("size", size).toUriString();
+        return UriComponentsBuilder.fromUriString(baseUrl).queryParam("page", page).queryParam("size", size)
+                .toUriString();
     }
 
     public static HttpHeaders generateSearchPaginationHttpHeaders(String query, Page page, String baseUrl) {
-        String escapedQuery;
-        try { escapedQuery = URLEncoder.encode(query, "UTF-8"); } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
+        String escapedQuery = null;
+        try {
+            escapedQuery = URLEncoder.encode(query, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage(), e);
         }
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Total-Count", Long.toString(page.getTotalElements()));
-        String link = "";
-        if ((page.getNumber() + 1) < page.getTotalPages()) {
-            link = "<" + generateUri(baseUrl, page.getNumber() + 1, page.getSize()) + QUEST+ escapedQuery + ">; rel=\"next\",";
-        }
-        // prev link
-        if ((page.getNumber()) > 0) { link += "<" + generateUri(baseUrl, page.getNumber() - 1, page.getSize()) + QUEST+ escapedQuery + ">; rel=\"prev\","; }
-        // last and first link
-        int lastPage = 0;
-        if (page.getTotalPages() > 0) { lastPage = page.getTotalPages() - 1;
-        }
-        link += "<" + generateUri(baseUrl, lastPage, page.getSize()) + QUEST + escapedQuery + ">; rel=\"last\",";
-        link += "<" + generateUri(baseUrl, 0, page.getSize()) + QUEST+ escapedQuery + ">; rel=\"first\"";
+        String link = getLink(page, baseUrl, escapedQuery);
         headers.add(HttpHeaders.LINK, link);
         return headers;
     }
 
+    private static String getLink(Page page, String baseUrl, String escapedQuery) {
+        String link = "";
+        if ((page.getNumber() + 1) < page.getTotalPages()) {
+            link = "<" + generateUri(baseUrl, page.getNumber() + 1, page.getSize()) + QUEST + escapedQuery
+                    + ">; rel=\"next\",";
+        }
+        int lastPage = 0;
+        if (page.getTotalPages() > 0) {
+            lastPage = page.getTotalPages() - 1;
+        }
+        link += "<" + generateUri(baseUrl, lastPage, page.getSize()) + QUEST + escapedQuery + ">; rel=\"last\",";
+        link += "<" + generateUri(baseUrl, 0, page.getSize()) + QUEST + escapedQuery + ">; rel=\"first\"";
+        return link;
+    }
 }
