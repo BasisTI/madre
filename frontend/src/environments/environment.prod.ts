c25f978801ace82a19199ce48cec3a18df52d840
Lucas Almeida <lucas.almeida@basis.com.br>
2018-05-16 16:38:13 -0300

    BASIS-38140 AMBIENTE

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: '/api',
   auth: {
     detailsUrl: '/api/user/details',
     loginUrl: '/api/login',
