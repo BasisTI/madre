1d45946a1016fdad03479219e1b36adaede42cf5
Jefferson de Oliveira Farias <jefferson.farias@basis.com.br>
2018-05-16 15:35:29 -0300

    BASIS-38151 Alteração de environments.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://madre.tst.basis.com.br:8080/api',
   auth: {
     detailsUrl: '/api/user/details',
     loginUrl: '/api/login',
