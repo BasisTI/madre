abee647f1e8dc523fbd162fcf11785b4d5264760
Brayan Quartezani Costa <brayan.costa@basis.com.br>
2018-05-17 12:34:30 -0300

    BASIS-38176 - Fix

--- cadastros-basicos/src/main/java/br/com/basis/madre/cadastros/web/rest/util/PaginationUtil.java
+++ cadastros-basicos/src/main/java/br/com/basis/madre/cadastros/web/rest/util/PaginationUtil.java
@@ -1,11 +1,13 @@
 package br.com.basis.madre.cadastros.web.rest.util;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Utility class for handling pagination.
