d4c32a990d26ab0400a939c990368e4aed8f48c6
Lucas Almeida <lucas.almeida@basis.com.br>
2018-05-21 17:26:17 -0300

    BASIS-38140 MadreUtil UnitTest

---
+++ cadastros-basicos/src/test/java/br/com/basis/madre/cadastros/util/MadreUtilTest.java
@@ -0,0 +1,42 @@
+package br.com.basis.madre.cadastros.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.time.LocalDate;
+
+import static br.com.basis.madre.cadastros.util.MadreUtil.getReportFooter;
+import static br.com.basis.madre.cadastros.util.MadreUtil.localDateTimeEmString;
+import static br.com.basis.madre.cadastros.util.MadreUtil.transformaLocalDateTimeEmString;
+
+@RunWith(PowerMockRunner.class)
+public class MadreUtilTest {
+    @InjectMocks
+    MadreUtil madreUtil;
+
+    @Mock
+    LocalDate localDate;
+
+  
+    @Test
+    public void getReportFooterTest() {
+        PowerMockito.mockStatic(MadreUtil.class);
+        String test = getReportFooter();
+    }
+
+    @Test
+    public void transformaLocalDateTimeStringTest(){
+       // PowerMockito.mockStatic(MadreUtil.class);
+        String test = transformaLocalDateTimeEmString(null);
+        test = transformaLocalDateTimeEmString(localDate);
+    }
+
+    @Test
+    public void localDateTimeEmStringTest(){
+        String test = localDateTimeEmString(null);
+    }
+}
