f7dbec9ae6f7ab4c82f8bb9158688081011950c3
Brayan Quartezani Costa <brayan.costa@basis.com.br>
2018-06-11 15:10:19 -0300

    BASIS-38157 - Teste + ajuste front

--- cadastros-basicos/src/test/java/br/com/basis/madre/cadastros/web/rest/PreCadastroResourceTest.java
+++ cadastros-basicos/src/test/java/br/com/basis/madre/cadastros/web/rest/PreCadastroResourceTest.java
@@ -3,44 +3,28 @@ package br.com.basis.madre.cadastros.web.rest;
 import br.com.basis.madre.cadastros.domain.PreCadastro;
 import br.com.basis.madre.cadastros.repository.PreCadastroRepository;
 import br.com.basis.madre.cadastros.service.PreCadastroService;
-import br.com.basis.madre.cadastros.web.rest.PreCadastroResource;
-import br.com.basis.madre.cadastros.web.rest.util.PaginationUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 
-import java.awt.print.Pageable;
-import java.util.List;
-
-@RunWith(PowerMockRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class PreCadastroResourceTest{
     @InjectMocks
-    private PreCadastroResource preCadastroResource;
+    PreCadastroResource preCadastroResource;
 
     @Mock
-    private PreCadastroService preCadastroService;
+    PreCadastroService preCadastroService;
 
     @Mock
-    private PreCadastroRepository preCadastroRepository;
-
-    @Mock
-    private PreCadastro preCadastro;
-
-    @Mock
-    private Pageable pageable;
-
-    @Mock
-    PaginationUtil paginationUtil;
+    PreCadastroRepository preCadastroRepository;
 
 
-    @Test
-    public void preCadastroResourceTest(){
-        new PreCadastroResource(preCadastroService,preCadastroRepository);
-    }
+@Test
+public  void tst(){new PreCadastroResource(preCadastroService,preCadastroRepository);}
+
 
     @Test
     public void deletePreCadastro(){
@@ -55,11 +39,6 @@ public class PreCadastroResourceTest{
     @Test
     public void getRelatorioExportacaoTest(){
         preCadastroResource.getRelatorioExportacao("teste","teste");
-    }
-
-    @Test
-    public void searchPreCadastroTest(){
-        ResponseEntity<List<PreCadastro>> teste = preCadastroResource.searchPreCadastros("*", null);
     }
 
 }
