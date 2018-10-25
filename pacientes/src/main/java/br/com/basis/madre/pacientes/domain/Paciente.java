409d850143afb65110544eca569bc55aaf2ce3ba
arturrenann <artur.barbosa@basis.com.br>
2018-10-25 12:48:51 -0300

    BASIS-1234 ERRO Sonar: import javax.persistence.*;

--- pacientes/src/main/java/br/com/basis/madre/pacientes/domain/Paciente.java
+++ pacientes/src/main/java/br/com/basis/madre/pacientes/domain/Paciente.java
@@ -9,7 +9,13 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.elasticsearch.annotations.Document;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
