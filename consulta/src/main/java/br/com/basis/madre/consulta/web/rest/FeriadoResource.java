01174f633eab933160875f91165e715dd6730c20
marlononorio <marlon.onorio@basis.com.br>
2020-03-28 18:04:17 -0300

    AMBULATORIO # manter feriado

--- consulta/src/main/java/br/com/basis/madre/consulta/web/rest/FeriadoResource.java
+++ consulta/src/main/java/br/com/basis/madre/consulta/web/rest/FeriadoResource.java
@@ -4,6 +4,7 @@ import br.com.basis.madre.consulta.service.FeriadoService;
 import br.com.basis.madre.consulta.service.dto.FeriadoDTO;
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
+import io.micrometer.core.annotation.Timed;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +35,7 @@ public class FeriadoResource {
     private static final String APP_NAME = "Madre";
 
     @PostMapping("/filter")
+    @Timed
     public ResponseEntity<Page<FeriadoDTO>> listar(@RequestBody FeriadoDTO feriadoDTO, @ApiParam Pageable pageable) {
         log.debug("REST request to get a page of Feriados");
         Page<FeriadoDTO> feriado = feriadoService.findAll(feriadoDTO, pageable);
@@ -42,6 +43,7 @@ public class FeriadoResource {
     }
 
     @GetMapping
+    @Timed
     public ResponseEntity<FeriadoDTO> buscaPorId(@RequestBody Long id) {
         log.debug("REST request to get Feriado : {}", id);
         Optional<FeriadoDTO> feriado = feriadoService.findById(id);
@@ -49,6 +51,7 @@ public class FeriadoResource {
     }
 
     @PostMapping
+    @Timed
     public ResponseEntity<FeriadoDTO> create(@RequestBody FeriadoDTO feriadoDTO) throws URISyntaxException {
         log.debug("REST request to save Feriado : {}", feriadoDTO);
         FeriadoDTO result = feriadoService.save(feriadoDTO);
@@ -58,6 +61,7 @@ public class FeriadoResource {
     }
 
     @DeleteMapping
+    @Timed
     public ResponseEntity delete(@RequestBody Long id) {
         log.debug("REST request to delete Feriado : {}", id);
         feriadoService.delete(id);
