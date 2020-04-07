f19c6b9cf8824aeb6a1eea7c30e7bcb7406140fe
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-07 11:38:20 -0300

    Import e export de módulos
    ReactiveForms, Forms e PrimeNG imports

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { PRIMENG_IMPORTS } from '../primeng-imports';
+
 import { JhiDateUtils } from './date-util.service';
 /* jhipster-needle-add-shared-service-import - JHipster will add shared services imports here */
 
-@NgModule({})
+@NgModule({
+    imports: [ReactiveFormsModule, FormsModule, PRIMENG_IMPORTS],
+    exports: [ReactiveFormsModule, FormsModule, PRIMENG_IMPORTS],
+})
 export class SharedModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: SharedModule,
-      providers: [
-        JhiDateUtils,
-        /* jhipster-needle-add-shared-services - JHipster will add shared services here */
-      ]
-    };
-  }
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [
+                JhiDateUtils,
+                /* jhipster-needle-add-shared-services - JHipster will add shared services here */
+            ],
+        };
+    }
 }
