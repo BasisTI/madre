5903d47d02a2cb5d9e34a4f65dbc68b324bfbdd4
Jocimar Neres <jocimar.silva@basis.com.br>
2020-04-07 15:31:56 -0300

    Adicionando componente "Cartão SUS"

--- frontend/src/app/pacientes/formulario-paciente/cartao-sus/cartao-sus.component.ts
+++ frontend/src/app/pacientes/formulario-paciente/cartao-sus/cartao-sus.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cartao-sus',
   templateUrl: './cartao-sus.component.html',
-  styles: []
+  styles: [],
 })
-export class CartaoSusComponent implements OnInit {
+export class CartaoSusComponent {
+  cartaoSUS: FormGroup = this.fb.group({
+    numero: [''],
+    justificativa: [''],
+    motivoCadastro: [''],
+    docReferencia: [''],
+    cartaoNacional: [''],
+    dataDeEntrada: [''],
+    dataDeNaturalizacao: [''],
+    portaria: [''],
+  });
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private fb: FormBuilder) {}
 }
