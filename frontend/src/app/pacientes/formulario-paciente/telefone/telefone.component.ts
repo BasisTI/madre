b9150be706f5508742d09aacb427753c60f8dc28
Mateus Mendonça <mateus.mendonca@basis.com.br>
2020-04-07 15:08:01 -0300

    adiciona componente "telefone"

--- frontend/src/app/pacientes/formulario-paciente/telefone/telefone.component.ts
+++ frontend/src/app/pacientes/formulario-paciente/telefone/telefone.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-telefone',
   templateUrl: './telefone.component.html',
-  styles: []
 })
-export class TelefoneComponent implements OnInit {
+export class TelefoneComponent {
+  constructor(private fb: FormBuilder) {}
+  telefone: FormGroup = this.fb.group({
+    tipo: [''],
+    telefone: [''],
+    observacao: [''],
+    DDD: [''],
+  });
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  fakeData = [
+    {
+      tipoDeTelefone: 'residencial',
+      telefone: '999999999',
+      observacao: 'teste',
+    },
+    { tipoDeTelefone: 'comercial', telefone: '9888888888', observacao: 'teste2' },
+  ];
 }
