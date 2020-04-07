b0cb2674480a8495403b3bf27beb71d610742a9a
Mateus Mendonça <mateus.mendonca@basis.com.br>
2020-04-07 15:07:32 -0300

    adiciona componente  "endereço"

--- frontend/src/app/pacientes/formulario-paciente/endereco/endereco.component.ts
+++ frontend/src/app/pacientes/formulario-paciente/endereco/endereco.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-endereco',
   templateUrl: './endereco.component.html',
-  styles: []
 })
-export class EnderecoComponent implements OnInit {
+export class EnderecoComponent {
+  endereco: FormGroup = this.fb.group({
+    municipio: [''],
+    CEP: [''],
+    UF: [''],
+    logradouro: [''],
+    Numero: [''],
+    Complemento: [''],
+    Bairro: [''],
+    tipo: [''],
+    corespondencia: [''],
+  });
 
-  constructor() { }
+  fakeData = [
+    {
+      municipio: 'São paulo',
+      CEP: '71717771771',
+      UF: 'SP',
+      logradouro: 'casa 12',
+      Numero: '22',
+      Complemento: 'teste',
+      Bairro: 'flamengo',
+      tipo: 'recidencia',
+      corespondencia: 'sim',
+    },
+    {
+      municipio: 'Acre ',
+      CEP: '727272772',
+      UF: 'AC',
+      logradouro: 'casa 33',
+      Numero: '33',
+      Complemento: 'teste2',
+      Bairro: 'Rio Branco',
+      tipo: 'comercial',
+      corespondencia: 'nao',
+    },
+  ];
 
-  ngOnInit() {
-  }
-
+  constructor(private fb: FormBuilder) {}
 }
