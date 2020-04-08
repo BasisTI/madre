a84cef698daf4295369f4ca4d40364c6022ec24b
Vinicius <amorimhavoc@gmail.com>
2020-04-08 12:58:31 -0300

    Adicionando componente de seleção e data em "Documentos"

--- frontend/src/app/pacientes/formulario-paciente/documentos/documentos.component.ts
+++ frontend/src/app/pacientes/formulario-paciente/documentos/documentos.component.ts
@@ -1,3 +1,4 @@
+import { ptBR } from './../../../shared/calendar.pt-br.locale';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
@@ -17,6 +18,33 @@ export class DocumentosComponent {
     cnh: [''],
     validadeCNH: [''],
   });
+
+  listaDeOrgaos = [
+    { label: 'Selecione', value: null },
+    { label: 'SSP-Secretária de Segurança Pública', value: 'Secretária de Segurança Pública' },
+    { label: 'Policia Federal', value: 'Policia Federal' },
+    { label: 'Ministério do Exército', value: 'Ministério do Exército' },
+    { label: 'Ministério da Marinha', value: 'Ministério da Marinha' },
+    { label: 'Conselho Regional de Contabilidade', value: 'Conselho Regional de Contabilidade' },
+    { label: 'Conselho Regional de Enfermagem', value: 'Conselho de Enfermagem' },
+  ];
+
+  listaDeUF = [
+    { label: 'Selecione', value: null },
+    { label: 'AC - ACRE', value: 'ACRE' },
+    { label: 'AL - ALAGOAS', value: 'ALAGOAS' },
+    { label: 'AM - AMAZONAS', value: 'AMAZONAS' },
+    { label: 'BA - BAHIA', value: 'BAHIA' },
+    { label: 'CE - CEARA', value: 'CEARA' },
+    { label: 'DF - DISTRITO FEDERAL', value: 'DISTRITO FEDERAL' },
+  ];
+
+  localizacao = ptBR;
+  maxDate = new Date();
+  yearRange = `1900:${this.maxDate.getFullYear()}`;
+
+  validade = ptBR;
+  yearValidade = '2010:2030';
 
   constructor(private fb: FormBuilder) {}
 }
