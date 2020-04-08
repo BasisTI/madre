a096b794eaad33ad34eeb3370d3471e38aef9f2e
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-08 15:17:47 -0300

    Adiciona componente "observacao" para o formulário de pacientes

---
+++ frontend/src/app/pacientes/formulario-paciente/observacao/observacao.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-observacao',
+  templateUrl: './observacao.component.html',
+  styles: [
+    `
+      textarea {
+        font-family: sans-serif;
+        font-size: 16px;
+      }
+    `,
+  ],
+})
+export class ObservacaoComponent {
+  observacao = new FormGroup({
+    valor: new FormControl(''),
+  });
+}
