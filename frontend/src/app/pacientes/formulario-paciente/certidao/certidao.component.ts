51fd5603e8843425d502b9b7ac5c8b8b84f67176
Jocimar Neres <jocimar.silva@basis.com.br>
2020-04-08 16:59:00 -0300

    Inserçao de mascaras e tooltips no componente Certidao.

--- frontend/src/app/pacientes/formulario-paciente/certidao/certidao.component.ts
+++ frontend/src/app/pacientes/formulario-paciente/certidao/certidao.component.ts
@@ -2,10 +2,23 @@ import { Component } from '@angular/core';
 
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { ptBR } from '../../../shared/calendar.pt-br.locale';
+
 @Component({
   selector: 'app-certidao',
   templateUrl: './certidao.component.html',
-  styles: [],
+  styles: [
+    `
+      .ui-widget.read-only:disabled {
+        opacity: 1;
+        background-color: #dddddd;
+      }
+
+      div {
+        margin: 3px;
+      }
+    `,
+  ],
 })
 export class CertidaoComponent {
   certidao: FormGroup = this.fb.group({
@@ -18,6 +31,18 @@ export class CertidaoComponent {
     dataDeEmissao: [''],
     numeroDaDN: [''],
   });
+
+  listaCertidao = [
+    { label: 'Selecione' },
+    { label: 'Nascimento', value: 'nascimento' },
+    { label: 'Casamento', value: 'casamento' },
+    { label: 'Separacao/Divorcio', value: 'separacaoDivorcio' },
+    { label: 'Indigena', value: 'indigena' },
+  ];
+
+  localizacao = ptBR;
+  maxDate = new Date();
+  yearRange = `1900:${this.maxDate.getFullYear()}`;
 
   constructor(private fb: FormBuilder) {}
 }
