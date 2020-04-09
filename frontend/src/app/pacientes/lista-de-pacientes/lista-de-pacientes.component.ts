ddce270de2dd1f63e8fdc4a44da799f6808b9bae
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-09 13:38:22 -0300

    Utiliza o serviço de pacientes para trazer registros do banco

--- frontend/src/app/pacientes/lista-de-pacientes/lista-de-pacientes.component.ts
+++ frontend/src/app/pacientes/lista-de-pacientes/lista-de-pacientes.component.ts
@@ -1,53 +1,20 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 
 import { BreadcrumbService } from '../../breadcrumb/breadcrumb.service';
-
-interface Paciente {
-  prontuario: string;
-  nome: string;
-  nomeDaMae: string;
-  dataDeNascimento: Date;
-  numeroDoCartaoSUS: string;
-}
+import { PacientesService } from '../pacientes.service';
 
 @Component({
   selector: 'app-lista-de-pacientes',
   templateUrl: './lista-de-pacientes.component.html',
 })
-export class ListaDePacientesComponent implements OnInit, OnDestroy {
+export class ListaDePacientesComponent implements OnInit, OnDestroy, AfterViewInit {
   searchUrl = '';
-  listaDePacientes: Paciente[] = [
-    {
-      prontuario: '123456789',
-      nomeDaMae: 'Maria da Silva',
-      nome: 'João da Silva',
-      dataDeNascimento: new Date(),
-      numeroDoCartaoSUS: '123456789000000',
-    },
-    {
-      prontuario: '123456789',
-      nomeDaMae: 'Maria da Silva',
-      nome: 'João da Silva',
-      dataDeNascimento: new Date(),
-      numeroDoCartaoSUS: '123456789000000',
-    },
-    {
-      prontuario: '123456789',
-      nomeDaMae: 'Maria da Silva',
-      nome: 'João da Silva',
-      dataDeNascimento: new Date(),
-      numeroDoCartaoSUS: '123456789000000',
-    },
-    {
-      prontuario: '123456789',
-      nomeDaMae: 'Maria da Silva',
-      nome: 'João da Silva',
-      dataDeNascimento: new Date(),
-      numeroDoCartaoSUS: '123456789000000',
-    },
-  ];
+  paginaDePacientes;
 
-  constructor(private breadcrumbService: BreadcrumbService) {}
+  constructor(
+    private breadcrumbService: BreadcrumbService,
+    private pacientesService: PacientesService,
+  ) {}
 
   ngOnInit(): void {
     this.breadcrumbService.setItems([{ label: 'Pacientes' }]);
@@ -55,5 +22,11 @@ export class ListaDePacientesComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.breadcrumbService.reset();
+  }
+
+  ngAfterViewInit(): void {
+    this.pacientesService.getListaDePacientes().then((response) => {
+      this.paginaDePacientes = response.content;
+    });
   }
 }
