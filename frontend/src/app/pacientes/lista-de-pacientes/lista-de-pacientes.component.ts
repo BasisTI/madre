825471a391cc4cda43f27c4f8bcbf8b974e131f8
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-09 10:40:05 -0300

    Adiciona datatable da basis

--- frontend/src/app/pacientes/lista-de-pacientes/lista-de-pacientes.component.ts
+++ frontend/src/app/pacientes/lista-de-pacientes/lista-de-pacientes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { BreadcrumbService } from '../../breadcrumb/breadcrumb.service';
 
 interface Paciente {
   prontuario: string;
@@ -12,7 +14,8 @@ interface Paciente {
   selector: 'app-lista-de-pacientes',
   templateUrl: './lista-de-pacientes.component.html',
 })
-export class ListaDePacientesComponent {
+export class ListaDePacientesComponent implements OnInit, OnDestroy {
+  searchUrl = '';
   listaDePacientes: Paciente[] = [
     {
       prontuario: '123456789',
@@ -43,4 +46,14 @@ export class ListaDePacientesComponent {
       numeroDoCartaoSUS: '123456789000000',
     },
   ];
+
+  constructor(private breadcrumbService: BreadcrumbService) {}
+
+  ngOnInit(): void {
+    this.breadcrumbService.setItems([{ label: 'Pacientes' }]);
+  }
+
+  ngOnDestroy(): void {
+    this.breadcrumbService.reset();
+  }
 }
