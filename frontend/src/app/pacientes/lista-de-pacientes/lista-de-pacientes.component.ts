143eab0ea6b72acd5888ce9c4064164bf363f8be
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-09 09:45:06 -0300

    Primeira versão da listagem de pacientes

--- frontend/src/app/pacientes/lista-de-pacientes/lista-de-pacientes.component.ts
+++ frontend/src/app/pacientes/lista-de-pacientes/lista-de-pacientes.component.ts
@@ -1,7 +1,46 @@
 import { Component } from '@angular/core';
 
+interface Paciente {
+  prontuario: string;
+  nome: string;
+  nomeDaMae: string;
+  dataDeNascimento: Date;
+  numeroDoCartaoSUS: string;
+}
+
 @Component({
   selector: 'app-lista-de-pacientes',
   templateUrl: './lista-de-pacientes.component.html',
 })
-export class ListaDePacientesComponent {}
+export class ListaDePacientesComponent {
+  listaDePacientes: Paciente[] = [
+    {
+      prontuario: '123456789',
+      nomeDaMae: 'Maria da Silva',
+      nome: 'João da Silva',
+      dataDeNascimento: new Date(),
+      numeroDoCartaoSUS: '123456789000000',
+    },
+    {
+      prontuario: '123456789',
+      nomeDaMae: 'Maria da Silva',
+      nome: 'João da Silva',
+      dataDeNascimento: new Date(),
+      numeroDoCartaoSUS: '123456789000000',
+    },
+    {
+      prontuario: '123456789',
+      nomeDaMae: 'Maria da Silva',
+      nome: 'João da Silva',
+      dataDeNascimento: new Date(),
+      numeroDoCartaoSUS: '123456789000000',
+    },
+    {
+      prontuario: '123456789',
+      nomeDaMae: 'Maria da Silva',
+      nome: 'João da Silva',
+      dataDeNascimento: new Date(),
+      numeroDoCartaoSUS: '123456789000000',
+    },
+  ];
+}
