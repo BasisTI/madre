63fb5e2708994567761a603df0d70762b95a118e
Jocimar Neres <jocimar.silva@basis.com.br>
2020-04-16 13:31:56 -0300

    Subindo alteração triagem

--- frontend/src/app/pacientes/triagem/triagem.component.ts
+++ frontend/src/app/pacientes/triagem/triagem.component.ts
@@ -1,5 +1,4 @@
 import { PacientesService } from './../pacientes.service';
-import { PacienteSummary } from './../models/paciente.summary';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BreadcrumbService } from '../../breadcrumb/breadcrumb.service';
 
@@ -20,10 +19,7 @@ import { BreadcrumbService } from '../../breadcrumb/breadcrumb.service';
     ],
 })
 export class TriagemComponent implements OnInit, OnDestroy {
-    constructor(
-        private breadcrumbService: BreadcrumbService,
-        private pacienteService: PacientesService,
-    ) {}
+    constructor(private breadcrumbService: BreadcrumbService) {}
     ngOnInit(): void {
         this.breadcrumbService.setItems([
             { label: 'Pacientes', routerLink: 'pacientes' },
