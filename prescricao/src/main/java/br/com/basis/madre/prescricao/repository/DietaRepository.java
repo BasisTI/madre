f2e73fb03c1386ee2da4146ff8065faa0d1a42ca
bragamateus <mateus.braga@basis.com.br>
2020-04-20 17:25:34 -0300

    consultas repository

--- prescricao/src/main/java/br/com/basis/madre/prescricao/repository/DietaRepository.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/repository/DietaRepository.java
@@ -1,12 +1,26 @@
 package br.com.basis.madre.prescricao.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.basis.madre.prescricao.domain.Dieta;
 
 @Repository
-
 public interface DietaRepository extends JpaRepository<Dieta, Long>{
+	@EntityGraph("dieta.itemDieta")
+	List<Dieta> findByIdPaciente(Long id);
+	
+	@EntityGraph("dieta.itemDieta")
+	Optional<Dieta> findById(Long id);
+	
+	@EntityGraph("dieta.itemDieta")
+	List<Dieta> findAll();
+	
+	Optional<Dieta> save(Optional<Dieta> dieta);
+
 
 }
--- prescricao/src/main/java/br/com/basis/madre/prescricao/repository/search/PacienteRepositorySearch.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/repository/search/PacienteRepositorySearch.java
@@ -1,5 +1,7 @@
 package br.com.basis.madre.prescricao.repository.search;
 
+import java.util.Optional;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import br.com.basis.madre.prescricao.domain.Paciente;
 
 @Repository
 public interface PacienteRepositorySearch extends ElasticsearchRepository<Paciente, Long>  {
-		
+	Optional<Paciente> findById(Long id); 
 }
