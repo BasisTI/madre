4b7c34d07fc0459434726552027abb1bfc495f97
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-23 10:50:16 -0300

    Transforma formGroup de endereços em formArray

--- frontend/src/app/pacientes/components/formulario-paciente/endereco/endereco.component.ts
+++ frontend/src/app/pacientes/components/formulario-paciente/endereco/endereco.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { MunicipioService } from './municipio.service';
 import { OPCOES_DE_TIPO_DE_TELEFONE } from '../../../models/dropdowns/opcoes-de-tipo-de-endereco';
 
@@ -9,13 +9,24 @@ import { OPCOES_DE_TIPO_DE_TELEFONE } from '../../../models/dropdowns/opcoes-de-
     styleUrls: ['./endereco.component.scss'],
 })
 export class EnderecoComponent {
-    @Input() enderecos: FormGroup;
+    @Input() enderecos: FormArray;
     opcoesDeTipoDeEndereco = OPCOES_DE_TIPO_DE_TELEFONE;
-    listaDeEnderecos = [];
+
+    endereco = this.fb.group({
+        municipio: ['', Validators.required],
+        cep: ['', Validators.required],
+        uf: [''],
+        logradouro: ['', Validators.required],
+        numero: ['', Validators.required],
+        complemento: [''],
+        bairro: ['', Validators.required],
+        tipo: ['', Validators.required],
+        correspondencia: ['', Validators.required],
+    });
 
     constructor(private fb: FormBuilder, public municipioService: MunicipioService) {}
 
     adicionarEnderecoALista() {
-        this.listaDeEnderecos.push(this.enderecos.value);
+        this.enderecos.push(this.endereco);
     }
 }
