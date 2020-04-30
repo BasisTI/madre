d323e602006617c892af110c058a6b44aac7991e
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-30 13:21:27 -0300

    Cria componente dropdown com autocomplete para Procedencia

--- frontend/src/app/internacao/components/procedencia/procedencia.component.ts
+++ frontend/src/app/internacao/components/procedencia/procedencia.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ProcedenciaService } from '@internacao/services/procedencia.service';
+import { FormGroup } from '@angular/forms';
+import { EntityAutoComplete } from '@shared/entity-autocomplete.component';
+import { Component, OnInit, Input } from '@angular/core';
+import { Procedencia } from '@internacao/models/procedencia';
 
 @Component({
-  selector: 'app-procedencia',
-  templateUrl: './procedencia.component.html',
-  styles: []
+    selector: 'app-procedencia',
+    templateUrl: './procedencia.component.html',
 })
-export class ProcedenciaComponent implements OnInit {
+export class ProcedenciaComponent implements OnInit, EntityAutoComplete {
+    @Input() public parentFormGroup: FormGroup;
+    @Input() public required: false;
+    @Input() public label = 'Procêdencia';
+    @Input() public name = 'procedencia';
+    public procedencias: Array<Procedencia>;
 
-  constructor() { }
+    constructor(private procedenciaService: ProcedenciaService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+        this.procedenciaService
+            .getProcedencias(true, 'descricao')
+            .subscribe((procedencias: Array<Procedencia>) => (this.procedencias = procedencias));
+    }
 
+    aoDigitar(evento: { originalEvent: any; query: string }): void {
+        this.procedenciaService
+            .getProcedenciasPorNome(evento.query, true, 'descricao')
+            .subscribe((procedencias: Array<Procedencia>) => (this.procedencias = procedencias));
+    }
+
+    aoDesfocar(): void {
+        if (!this.parentFormGroup.get(this.name).value) {
+            this.parentFormGroup.get(this.name).setValue(null);
+        }
+    }
 }
