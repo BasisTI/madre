9546de55fc6d0349348c34ea6ee1a897fe3b7874
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-04-30 13:20:52 -0300

    Cria componente dropdown com autocomplete para CaraterDaInternacao

--- frontend/src/app/internacao/components/carater-da-internacao/carater-da-internacao.component.ts
+++ frontend/src/app/internacao/components/carater-da-internacao/carater-da-internacao.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { EntityAutoComplete } from '@shared/entity-autocomplete.component';
+import { Component, OnInit, Input } from '@angular/core';
+import { CaraterDaInternacaoService } from '@internacao/services/carater-da-internacao.service';
+import { CaraterDaInternacao } from '@internacao/models/carater-da-internacao';
 
 @Component({
-  selector: 'app-carater-da-internacao',
-  templateUrl: './carater-da-internacao.component.html',
-  styles: []
+    selector: 'app-carater-da-internacao',
+    templateUrl: './carater-da-internacao.component.html',
+    styles: [],
 })
-export class CaraterDaInternacaoComponent implements OnInit {
+export class CaraterDaInternacaoComponent implements OnInit, EntityAutoComplete {
+    @Input() public parentFormGroup: FormGroup;
+    @Input() public required = false;
+    @Input() public label = 'Caráter da Internação';
+    @Input() public name = 'caraterDaInternacao';
+    public caracteresDeIntenacao: Array<CaraterDaInternacao>;
 
-  constructor() { }
+    constructor(private caraterDaInternacaoService: CaraterDaInternacaoService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+        this.caraterDaInternacaoService
+            .getCaracteresDaInternacao(true, 'nome')
+            .subscribe(
+                (caracteresDeIntenacao: Array<CaraterDaInternacao>) =>
+                    (this.caracteresDeIntenacao = caracteresDeIntenacao),
+            );
+    }
 
+    aoDigitar(evento: { originalEvent: any; query: string }): void {
+        this.caraterDaInternacaoService
+            .getCaracteresDaInternacaoPorNome(evento.query, true, 'nome')
+            .subscribe(
+                (caracteresDeIntenacao: Array<CaraterDaInternacao>) =>
+                    (this.caracteresDeIntenacao = caracteresDeIntenacao),
+            );
+    }
+
+    aoDesfocar(): void {
+        if (!this.parentFormGroup.get(this.name).value) {
+            this.parentFormGroup.get(this.name).setValue(null);
+        }
+    }
 }
