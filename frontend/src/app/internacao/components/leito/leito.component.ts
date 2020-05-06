9f29fe7b46daeb982dee999f4a83b8f70a767645
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 15:23:02 -0300

    Adiciona busca de leitos desocupados por nome

--- frontend/src/app/internacao/components/leito/leito.component.ts
+++ frontend/src/app/internacao/components/leito/leito.component.ts
@@ -1,11 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { LeitoService } from './../../services/leito.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Leito } from '@internacao/models/leito';
+import { EntityAutoComplete } from '@shared/entity-autocomplete.component';
 
 @Component({
     selector: 'app-leito',
     templateUrl: './leito.component.html',
 })
-export class LeitoComponent implements OnInit {
-    constructor() {}
+export class LeitoComponent implements OnInit, EntityAutoComplete {
+    @Input() public parentFormGroup: FormGroup;
+    @Input() public required = false;
+    @Input() public label = 'Leito';
+    @Input() public name = 'leito';
+    public leitos = new Array<Leito>();
 
-    ngOnInit() {}
+    constructor(private leitoService: LeitoService) {}
+
+    ngOnInit() {
+        this.leitoService
+            .getLeitosDesocupados()
+            .subscribe((leitos: Array<Leito>) => (this.leitos = leitos));
+    }
+
+    aoDigitar(evento: { originalEvent: any; query: string }): void {
+        this.leitoService
+            .getLeitosDesocupadosPorNome(evento.query)
+            .subscribe((leitos: Array<Leito>) => (this.leitos = leitos));
+    }
+
+    aoDesfocar(): void {
+        if (!this.parentFormGroup.get(this.name).value) {
+            this.parentFormGroup.get(this.name).setValue(null);
+        }
+    }
 }
