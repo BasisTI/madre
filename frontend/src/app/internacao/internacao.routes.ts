b294956c6d6d39af05978d7b4214fcd3c22b39ec
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 13:04:06 -0300

    Adiciona rota para componente de reservação de leito

--- frontend/src/app/internacao/internacao.routes.ts
+++ frontend/src/app/internacao/internacao.routes.ts
@@ -1,3 +1,4 @@
+import { ReservaDeLeitoComponent } from './components/reserva-de-leito/reserva-de-leito.component';
 import { InternacaoDePacienteComponent } from './components/internacao-de-paciente/internacao-de-paciente.component';
 import { Routes } from '@angular/router';
 import { SolicitacaoDeInternacaoComponent } from './components/solicitacao-de-internacao/solicitacao-de-internacao.component';
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'internacao-de-paciente/:id',
         component: InternacaoDePacienteComponent,
+    },
+    {
+        path: 'reserva-de-leito',
+        component: ReservaDeLeitoComponent,
     },
 ];
