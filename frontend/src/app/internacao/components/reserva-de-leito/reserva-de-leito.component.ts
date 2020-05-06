c7f6e61e25ccd6010409feb43fbf79ea12958337
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 13:07:32 -0300

    Adiciona breadcrumb para reservação de leito

--- frontend/src/app/internacao/components/reserva-de-leito/reserva-de-leito.component.ts
+++ frontend/src/app/internacao/components/reserva-de-leito/reserva-de-leito.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { BreadcrumbService } from '@breadcrumb/breadcrumb.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-reserva-de-leito',
     templateUrl: './reserva-de-leito.component.html',
 })
-export class ReservaDeLeitoComponent implements OnInit {
-    constructor() {}
+export class ReservaDeLeitoComponent implements OnInit, OnDestroy {
+    constructor(private breadcrumbService: BreadcrumbService) {}
 
-    ngOnInit() {}
+    ngOnInit(): void {
+        this.breadcrumbService.setItems([
+            {
+                label: 'Internação',
+            },
+            {
+                label: 'Reservar Leito',
+                routerLink: 'reserva-de-leito',
+            },
+        ]);
+    }
+
+    ngOnDestroy(): void {
+        this.breadcrumbService.reset();
+    }
 }
