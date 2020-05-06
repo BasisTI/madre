6f679ec659bce9d6db96a4bc10ad2b21726382a4
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 13:44:06 -0300

    Adiciona estrutura inicial do formulário

--- frontend/src/app/internacao/components/reserva-de-leito/reserva-de-leito.component.ts
+++ frontend/src/app/internacao/components/reserva-de-leito/reserva-de-leito.component.ts
@@ -1,12 +1,34 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { BreadcrumbService } from '@breadcrumb/breadcrumb.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ConfiguracaoParaCalendarioPrimeNG, ptBR } from '@shared/p-calendar.config';
 
 @Component({
     selector: 'app-reserva-de-leito',
     templateUrl: './reserva-de-leito.component.html',
+    styleUrls: ['./reserva-de-leito.component.scss'],
 })
 export class ReservaDeLeitoComponent implements OnInit, OnDestroy {
-    constructor(private breadcrumbService: BreadcrumbService) {}
+    public pCalendarConfig: ConfiguracaoParaCalendarioPrimeNG = {
+        anosDisponiveis: '1900:2100',
+        formatoDeData: 'dd/mm/yyyy',
+        localidade: {
+            ...ptBR,
+            today: 'Agora',
+            clear: 'Limpar',
+        },
+    };
+
+    public formGroup: FormGroup = this.fb.group({
+        leito: ['', Validators.required],
+        situacao: ['', Validators.required],
+        tipoDaReserva: [''],
+        origem: [''],
+        dataDoLancamento: [new Date(), Validators.required],
+        justificativa: [''],
+    });
+
+    constructor(private breadcrumbService: BreadcrumbService, private fb: FormBuilder) {}
 
     ngOnInit(): void {
         this.breadcrumbService.setItems([
@@ -22,5 +44,9 @@ export class ReservaDeLeitoComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this.breadcrumbService.reset();
+    }
+
+    reservarLeito(evento) {
+        console.log(evento);
     }
 }
