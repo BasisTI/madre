31d5ec8a01f3754e401f98595aec9a6ce581dcf2
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 13:02:23 -0300

    Adiciona componente para reservação de leito

---
+++ frontend/src/app/internacao/components/reserva-de-leito/reserva-de-leito.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-reserva-de-leito',
+    templateUrl: './reserva-de-leito.component.html',
+})
+export class ReservaDeLeitoComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
