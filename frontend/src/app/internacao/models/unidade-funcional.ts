ced960426f16902cbe561962e5a60c1b1e581f50
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 14:44:30 -0300

    Adiciona modelo para unidade funcional

---
+++ frontend/src/app/internacao/models/unidade-funcional.ts
@@ -0,0 +1,9 @@
+export interface IUnidadeFuncional {
+    id?: number;
+    nome?: string;
+}
+
+export class UnidadeFuncional implements IUnidadeFuncional {
+    public id?: number;
+    public nome?: string;
+}
