96ca8523fd232be6a4c45d5034de2badce5a35a0
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 14:49:34 -0300

    Adiciona componente para busca de leito

---
+++ frontend/src/app/internacao/components/leito/leito.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-leito',
+    templateUrl: './leito.component.html',
+})
+export class LeitoComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
