117fa2d48a8b2135ca45c96acc7382210264ab85
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-06 15:22:07 -0300

    (Frontend) Adiciona classe de serviço para entidade Leito

---
+++ frontend/src/app/internacao/services/leito.service.ts
@@ -0,0 +1,35 @@
+import { Leito } from './../models/leito';
+import { api } from './../api';
+import { Observable } from 'rxjs';
+import { HttpParams, HttpClient } from '@angular/common/http';
+import { EntityService } from '@shared/entity.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LeitoService implements EntityService {
+    private readonly resource = `${api}/leitos`;
+
+    constructor(private client: HttpClient) {}
+
+    getResource<T>(params?: HttpParams): Observable<T> {
+        if (params) {
+            return this.client.get<T>(this.resource, { params });
+        }
+
+        return this.client.get<T>(this.resource);
+    }
+
+    getLeitosDesocupados(): Observable<Array<Leito>> {
+        return this.client.get<Array<Leito>>(`${this.resource}/desocupados`, {
+            params: new HttpParams().set('sort', 'nome'),
+        });
+    }
+
+    getLeitosDesocupadosPorNome(nome: string): Observable<Array<Leito>> {
+        return this.client.get<Array<Leito>>(`${this.resource}/desocupados`, {
+            params: new HttpParams().set('sort', 'nome').set('nome', nome),
+        });
+    }
+}
