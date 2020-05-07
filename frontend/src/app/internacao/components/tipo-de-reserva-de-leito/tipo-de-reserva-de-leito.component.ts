2b63c48c1e196d7fa8d78355847e58a03e0748a7
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-07 10:32:56 -0300

    Adiciona componete de tipo de reserva de leito

---
+++ frontend/src/app/internacao/components/tipo-de-reserva-de-leito/tipo-de-reserva-de-leito.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-tipo-de-reserva-de-leito',
+    templateUrl: './tipo-de-reserva-de-leito.component.html',
+})
+export class TipoDeReservaDeLeitoComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
