e2e883151df5824f96468938f0c94c3d91d77f2c
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-07 13:14:12 -0300

    Adiciona serviço para bloqueio de leito

--- frontend/src/app/internacao/services/bloqueio-de-leito.service.ts
+++ frontend/src/app/internacao/services/bloqueio-de-leito.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
     providedIn: 'root',
 })
 export class BloqueioDeLeitoService implements EntityService {
-    private readonly resource = `${api}`;
+    private readonly resource = `${api}/bloqueios-de-leito`;
 
     constructor(private client: HttpClient) {}
 
@@ -18,6 +18,19 @@ export class BloqueioDeLeitoService implements EntityService {
         }
 
         return this.client.get<T>(this.resource);
+    }
+
+    bloquearLeito(bloqueio): Observable<any> {
+        console.log(bloqueio);
+
+        const bloqueioDTO = {
+            dataDoLancamento: bloqueio.dataDoLancamento,
+            justificativa: bloqueio.justificativa,
+            leitoId: bloqueio.leito.id,
+            motivoId: bloqueio.motivo.id,
+        };
+
+        return this.client.post<any>(this.resource, bloqueioDTO);
     }
 
     getApi(): string {
