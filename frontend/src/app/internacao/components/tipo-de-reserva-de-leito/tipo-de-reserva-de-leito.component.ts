d1f5accee9aa7728f06be905437c9ee87818fc30
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-07 10:39:53 -0300

    Implementa a interface de serviço de entidade

--- frontend/src/app/internacao/components/tipo-de-reserva-de-leito/tipo-de-reserva-de-leito.component.ts
+++ frontend/src/app/internacao/components/tipo-de-reserva-de-leito/tipo-de-reserva-de-leito.component.ts
@@ -1,11 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { TipoDeReservaDeLeito } from '@internacao/models/tipo-de-reserva-de-leito';
+import { EntityAutoComplete } from './../../../shared/entity-autocomplete.component';
+import { TipoDaReservaDeLeitoService } from './../../services/tipo-da-reserva-de-leito.service';
 
 @Component({
     selector: 'app-tipo-de-reserva-de-leito',
     templateUrl: './tipo-de-reserva-de-leito.component.html',
 })
-export class TipoDeReservaDeLeitoComponent implements OnInit {
-    constructor() {}
+export class TipoDeReservaDeLeitoComponent implements OnInit, EntityAutoComplete {
+    @Input() public parentFormGroup: FormGroup;
+    @Input() public required = false;
+    @Input() public label = 'Tipo da Reserva';
+    @Input() public name = 'tipoDaReserva';
+    public tiposDeReservaDeLeito = new Array<TipoDeReservaDeLeito>();
 
-    ngOnInit() {}
+    constructor(private tipoDaReservaDeLeitoService: TipoDaReservaDeLeitoService) {}
+
+    ngOnInit() {
+        this.tipoDaReservaDeLeitoService
+            .getTiposDeReserva()
+            .subscribe(
+                (tiposDeReservaDeLeito: Array<TipoDeReservaDeLeito>) =>
+                    (this.tiposDeReservaDeLeito = tiposDeReservaDeLeito),
+            );
+    }
+
+    aoDigitar(evento: { originalEvent: any; query: string }): void {
+        this.tipoDaReservaDeLeitoService
+            .getTiposDeReservaPorNome(evento.query)
+            .subscribe(
+                (tiposDeReservaDeLeito: Array<TipoDeReservaDeLeito>) =>
+                    (this.tiposDeReservaDeLeito = tiposDeReservaDeLeito),
+            );
+    }
+
+    aoDesfocar(): void {
+        if (!this.parentFormGroup.get(this.name).value) {
+            this.parentFormGroup.get(this.name).setValue(null);
+        }
+    }
 }
