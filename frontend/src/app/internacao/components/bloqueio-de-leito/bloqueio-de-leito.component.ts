164065bbd5d83b3228d5b7a1a9063de88140251c
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-07 12:10:42 -0300

    Adiciona implementação do componente de bloqueio de leito

--- frontend/src/app/internacao/components/bloqueio-de-leito/bloqueio-de-leito.component.ts
+++ frontend/src/app/internacao/components/bloqueio-de-leito/bloqueio-de-leito.component.ts
@@ -1,12 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Leito } from '@internacao/models/leito';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfiguracaoParaCalendarioPrimeNG, ptBR } from '@shared/p-calendar.config';
+import { BreadcrumbService } from '@breadcrumb/breadcrumb.service';
+import { BloqueioDeLeitoService } from '@internacao/services/bloqueio-de-leito.service';
 
 @Component({
     selector: 'app-bloqueio-de-leito',
     templateUrl: './bloqueio-de-leito.component.html',
     styleUrls: ['./bloqueio-de-leito.component.scss'],
 })
-export class BloqueioDeLeitoComponent implements OnInit {
-    constructor() {}
+export class BloqueioDeLeitoComponent implements OnInit, OnDestroy {
+    public pCalendarConfig: ConfiguracaoParaCalendarioPrimeNG = {
+        anosDisponiveis: '1900:2100',
+        formatoDeData: 'dd/mm/yyyy',
+        localidade: {
+            ...ptBR,
+            today: 'Agora',
+            clear: 'Limpar',
+        },
+    };
 
-    ngOnInit() {}
+    public formGroup: FormGroup = this.fb.group({
+        leito: ['', Validators.required],
+        situacao: [{ value: '', disabled: true }, Validators.required],
+        motivo: ['', Validators.required],
+        dataDoLancamento: [new Date(), Validators.required],
+        justificativa: [''],
+    });
+
+    constructor(
+        private breadcrumbService: BreadcrumbService,
+        private fb: FormBuilder,
+        private bloqueioDeLeitoService: BloqueioDeLeitoService,
+    ) {}
+
+    ngOnInit(): void {
+        this.breadcrumbService.setItems([
+            {
+                label: 'Internação',
+            },
+            {
+                label: 'Bloquear Leito',
+                routerLink: 'bloqueio-de-leito',
+            },
+        ]);
+    }
+
+    ngOnDestroy(): void {
+        this.breadcrumbService.reset();
+    }
+
+    aoSelecionarLeito(leito: Leito): void {
+        this.formGroup.controls.situacao.setValue(leito.situacao.nome);
+    }
+
+    aoDesfocarLeito(): void {
+        this.formGroup.controls.situacao.setValue(null);
+    }
+
+    reservarLeito(): void {
+        console.log(this.formGroup.value);
+    }
 }
