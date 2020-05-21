d5d8ac1edaa6588bd42e2e5d3c66573e5c6c7c92
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-21 10:58:07 -0300

    Adiciona componente para visualização de eventos pelo calendário

---
+++ frontend/src/app/internacao/components/leito/calendario.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EventoCalendario } from '@internacao/models/evento-calendario';
+import { EventoLeitoService } from '@internacao/services/evento-leito.service';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+@Component({
+    selector: 'app-calendario',
+    templateUrl: './calendario.component.html',
+})
+export class CalendarioComponent implements OnInit {
+    events: Array<EventoCalendario>;
+
+    options: any;
+
+    constructor(private eventoLeitoService: EventoLeitoService) {}
+
+    ngOnInit(): void {
+        this.options = {
+            plugins: [dayGridPlugin],
+            defaultDate: '2020-05-21',
+            header: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay',
+            },
+        };
+        this.eventoLeitoService.obterEventosCalendario().subscribe((eventos) => {
+            this.events = eventos;
+        });
+    }
+}
