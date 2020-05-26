3cc9bfc516ed86d9bb22a6c0614c736209b8a5ca
bragamateus <mateus.braga@basis.com.br>
2020-05-26 10:06:55 -0300

    formata código - BASIS-127312

--- frontend/src/app/prescricao-medica/prescricao-medica.service.ts
+++ frontend/src/app/prescricao-medica/prescricao-medica.service.ts
@@ -3,15 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class PrescricaoMedicaService{
+export class PrescricaoMedicaService {
     baseUrl = '/prescricao/api';
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) { }
 
-    buscarIdPaciente(id: number): Observable<any>{
+    buscarIdPaciente(id: number): Observable<any> {
         return this.http.get(`${this.baseUrl}/pacientes/${id}`);
     }
 
     listarPacientes(): Observable<any> {
-       return this.http.get(`${this.baseUrl}/pacientes`);
+        return this.http.get(`${this.baseUrl}/pacientes`);
     }
 }
