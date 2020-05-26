ec612c13546887d967731b76091cae70ab88155b
bragamateus <mateus.braga@basis.com.br>
2020-05-26 15:14:37 -0300

    organizando imports - BASIS-127312

--- frontend/src/app/prescricao-medica/prescricao-medica.module.ts
+++ frontend/src/app/prescricao-medica/prescricao-medica.module.ts
@@ -1,18 +1,22 @@
-import { medicamentoRoute } from './medicamento/medicamento.routes';
 import { PrescricaoMedicaDietaModule } from './dieta/prescricao-medica-dieta.module';
+import { ProcedimentoEspecialModule } from './procedimento-especial/procedimento-especial.module';
+
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { SharedModule } from './../shared/shared.module';
 
-import { PrescricaoMedicaComponent } from './prescricao-medica.component';
+import { medicamentoRoute } from './medicamento/medicamento.routes';
+import { procedimentoEspecialRoute } from './procedimento-especial/procedimento-especial.routes';
+
 import { prescricaoMedica } from './prescricao-medica.router';
 import { PrescricaoMedicaService } from './prescricao-medica.service';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { PrescricaoMedicaComponent } from './prescricao-medica.component';
 import { MedicamentoComponent } from './medicamento/medicamento.component';
 import { ProcedimentoEspecialComponent } from './procedimento-especial/procedimento-especial.component';
-import { procedimentoEspecialRoute } from './procedimento-especial/procedimento-especial.routes';
 
 
 
@@ -25,6 +29,7 @@ import { procedimentoEspecialRoute } from './procedimento-especial/procedimento-
         HttpClientModule,
         ReactiveFormsModule,
         PrescricaoMedicaDietaModule,
+        ProcedimentoEspecialModule
     ],
     declarations: [
         PrescricaoMedicaComponent,
