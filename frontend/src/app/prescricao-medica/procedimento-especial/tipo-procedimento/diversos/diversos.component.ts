48a8d62c016ad36afa9cd504400f5cc598d86e26
bragamateus <mateus.braga@basis.com.br>
2020-05-26 10:07:36 -0300

    componente tipo de procedimento especial - BASIS-127312

---
+++ frontend/src/app/prescricao-medica/procedimento-especial/tipo-procedimento/diversos/diversos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diversos',
+  templateUrl: './diversos.component.html',
+  styleUrls: ['./diversos.component.css']
+})
+export class DiversosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
