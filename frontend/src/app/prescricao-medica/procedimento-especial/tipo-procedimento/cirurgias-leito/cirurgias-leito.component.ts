665ef4540a39165b744ede0892c1b626bda92105
bragamateus <mateus.braga@basis.com.br>
2020-05-26 15:13:36 -0300

    criando componente cirurgias-leito BASIS-127312

---
+++ frontend/src/app/prescricao-medica/procedimento-especial/tipo-procedimento/cirurgias-leito/cirurgias-leito.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-cirurgias-leito',
+    templateUrl: './cirurgias-leito.component.html'
+})
+export class CirurgiasLeitoComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
