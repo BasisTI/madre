d67229e5efcf813264a213cfc59f0ac182bf2642
bragamateus <mateus.braga@basis.com.br>
2020-05-26 15:11:58 -0300

    criando componene ortese-protese - BASIS-127312

---
+++ frontend/src/app/prescricao-medica/procedimento-especial/tipo-procedimento/ortese-protese/ortese-protese.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-ortese-protese',
+    templateUrl: './ortese-protese.component.html'
+})
+export class OrteseProteseComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
