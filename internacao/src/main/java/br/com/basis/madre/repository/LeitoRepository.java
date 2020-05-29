d80572b24142f0359912ae934eeaecc186c8e868
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-29 17:06:21 -0300

    Substitui uso do nome do tipo do evento pelo id

--- internacao/src/main/java/br/com/basis/madre/repository/LeitoRepository.java
+++ internacao/src/main/java/br/com/basis/madre/repository/LeitoRepository.java
@@ -1,6 +1,7 @@
 package br.com.basis.madre.repository;
 
 import br.com.basis.madre.domain.Leito;
+import br.com.basis.madre.domain.enumeration.CodigoDoTipoEventoLeito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,20 +12,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LeitoRepository extends JpaRepository<Leito, Long> {
 
-    @Query("select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = 'Reserva' and current_date between evento.dataInicio and evento.dataFim")
+    static interface Queries {
+        String obterTodosOsLeitosReservados = "select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = " + CodigoDoTipoEventoLeito.Constants.RESERVA_ID + " and current_date between evento.dataInicio and evento.dataFim";
+        String obterTodosOsLeitosBloqueados = "select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.id = " + CodigoDoTipoEventoLeito.Constants.BLOQUEIO_ID + " and current_date between evento.dataInicio and evento.dataFim";
+        String obterTodosOsLeitosOcupados = "select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.id = " + CodigoDoTipoEventoLeito.Constants.OCUPACAO_ID + " and evento.dataFim is null";
+        String obterTodosOsLeitosNaoLiberados = "select leito from EventoLeito evento join evento.leito leito where not evento.tipoDoEvento.id = " + CodigoDoTipoEventoLeito.Constants.LIBERACAO_ID + " and current_date between evento.dataInicio and evento.dataFim or evento.tipoDoEvento.nome = " + CodigoDoTipoEventoLeito.Constants.OCUPACAO_ID;
+        String obterTodosOsLeitosLiberados = "select leito from EventoLeito evento right join evento.leito leito where evento.dataFim > current_date or evento is null";
+    }
+
+    @Query(Queries.obterTodosOsLeitosReservados)
     Page<Leito> obterTodosOsLeitosReservados(Pageable pageable);
 
-    @Query("select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = 'Bloqueio' and current_date between evento.dataInicio and evento.dataFim")
+    @Query(Queries.obterTodosOsLeitosBloqueados)
     Page<Leito> obterTodosOsLeitosBloqueados(Pageable pageable);
 
-    @Query("select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = 'Ocupacao' and evento.dataFim is null")
+    @Query(Queries.obterTodosOsLeitosOcupados)
     Page<Leito> obterTodosOsLeitosOcupados(Pageable pageable);
 
-    @Query("select leito from EventoLeito evento join evento.leito leito\n" +
-        "where not evento.tipoDoEvento.nome = 'Liberação'\n" +
-        "and current_date between evento.dataInicio and evento.dataFim or evento.tipoDoEvento.nome = 'Ocupacao'")
+    @Query(Queries.obterTodosOsLeitosNaoLiberados)
     Page<Leito> obterTodosOsLeitosNaoLiberados(Pageable pageable);
 
-    @Query("select leito from EventoLeito evento right join evento.leito leito where evento.dataFim > current_date or evento is null")
+    @Query(Queries.obterTodosOsLeitosLiberados)
     Page<Leito> obterTodosOsLeitosLiberados(Pageable pageable);
 }
