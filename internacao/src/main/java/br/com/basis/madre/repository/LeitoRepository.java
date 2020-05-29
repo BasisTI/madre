87ec0ba596d6f3dcde46fea0226a6bf42b2abd3a
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-05-29 16:17:14 -0300

    Corrige filtragem de leito

--- internacao/src/main/java/br/com/basis/madre/repository/LeitoRepository.java
+++ internacao/src/main/java/br/com/basis/madre/repository/LeitoRepository.java
@@ -17,15 +17,14 @@ public interface LeitoRepository extends JpaRepository<Leito, Long> {
     @Query("select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = 'Bloqueio' and current_date between evento.dataInicio and evento.dataFim")
     Page<Leito> obterTodosOsLeitosBloqueados(Pageable pageable);
 
-    @Query("select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = 'Ocupação' and evento.dataFim is null")
+    @Query("select leito from EventoLeito evento join evento.leito leito where evento.tipoDoEvento.nome = 'Ocupacao' and evento.dataFim is null")
     Page<Leito> obterTodosOsLeitosOcupados(Pageable pageable);
 
     @Query("select leito from EventoLeito evento join evento.leito leito\n" +
         "where not evento.tipoDoEvento.nome = 'Liberação'\n" +
-        "and current_date between evento.dataInicio and evento.dataFim or evento.tipoDoEvento.nome = 'Ocupação'")
+        "and current_date between evento.dataInicio and evento.dataFim or evento.tipoDoEvento.nome = 'Ocupacao'")
     Page<Leito> obterTodosOsLeitosNaoLiberados(Pageable pageable);
 
-    @Query("select leito from EventoLeito evento right join evento.leito leito\n" +
-        "where current_date > evento.dataFim or evento is null")
+    @Query("select leito from EventoLeito evento right join evento.leito leito where evento.dataFim > current_date or evento is null")
     Page<Leito> obterTodosOsLeitosLiberados(Pageable pageable);
 }
