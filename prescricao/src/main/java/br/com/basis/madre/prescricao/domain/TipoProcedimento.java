467e94a96cc4a74ec505768926bba56acc88ff07
Mateus Braga <mateus.braga@basis.com.br>
2020-06-01 13:14:04 -0300

    Criando campo que estava faltando - BASIS-127312

--- prescricao/src/main/java/br/com/basis/madre/prescricao/domain/TipoProcedimento.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/domain/TipoProcedimento.java
@@ -1,11 +1,15 @@
 package br.com.basis.madre.prescricao.domain;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -16,6 +20,8 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -44,6 +50,11 @@ public class TipoProcedimento implements Serializable {
     @Size(max = 100)
     @Column(name = "descricao", length = 100, nullable = false)
     private String descricao;
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "tipoProcedimento", cascade = CascadeType.ALL)
+	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+	private Set<ItemPrescricaoProcedimento> itemPrescricaoProcedimento = new HashSet<>();
 
 
     public TipoProcedimento descricao(String descricao) {
