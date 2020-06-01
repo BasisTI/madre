2572ab452b303eb1e85d7e86fd0cdb2fa7492691
Mateus Braga <mateus.braga@basis.com.br>
2020-06-01 13:15:22 -0300

    Campo no DTO - BASIS-127312

--- prescricao/src/main/java/br/com/basis/madre/prescricao/service/dto/TipoProcedimentoDTO.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/service/dto/TipoProcedimentoDTO.java
@@ -1,9 +1,14 @@
 package br.com.basis.madre.prescricao.service.dto;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import br.com.basis.madre.prescricao.domain.ItemPrescricaoProcedimento;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -22,6 +27,9 @@ public class TipoProcedimentoDTO implements Serializable {
     @Size(max = 100)
     @ApiModelProperty(value = "descrição de procedimentos especiais diversos", required = true)
     private String descricao;
+    
+    @JsonIgnore
+    private Set<ItemPrescricaoProcedimento> itemPrescricaoProcedimento = new HashSet<>();
 
 
 }
