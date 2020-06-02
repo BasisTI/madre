d233d4838d6105e0bed0e2d4e58ef595f9d7e0f2
Mateus Braga <mateus.braga@basis.com.br>
2020-06-02 17:10:19 -0300

    Endpoint para obter paciente - BASIS-128436

--- prescricao/src/main/java/br/com/basis/madre/prescricao/web/rest/PacienteResource.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/web/rest/PacienteResource.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.javafaker.Faker;
 
 import br.com.basis.madre.prescricao.domain.Paciente;
 import br.com.basis.madre.prescricao.repository.search.PacienteRepositorySearch;
+import br.com.basis.madre.prescricao.service.PacienteService;
+import io.github.jhipster.web.util.PaginationUtil;
 
 @RestController
 @RequestMapping("/api/pacientes")
@@ -32,6 +36,8 @@ public class PacienteResource {
 	@Autowired
 	private PacienteRepositorySearch pacienteRepositorySearch;
 	
+	private PacienteService pacienteService;
+			
 	private Faker faker = new Faker(new Locale("pt-BR"));
 	
 	@GetMapping()
@@ -71,10 +77,18 @@ public class PacienteResource {
 				paciente.setId(faker.number().randomNumber());
 				
 				pacienteRepositorySearch.save(paciente);
+				
 			}
 	    	
 	    	return "OK";
 	  
 	    }
 	
+	@GetMapping("/pacientes")
+    public ResponseEntity<List<Paciente>> obterTodosPacientes(Pageable pageable) {
+        Page<Paciente> page = pacienteService.obterTodosPacientes(pageable);
+        HttpHeaders headers = PaginationUtil
+            .generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
 }
