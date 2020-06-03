2fa70b1d0b9e1e9685acd9c95203e9249a64a31d
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-06-03 11:17:30 -0300

    Espelha a classe pacientes.EventoPaciente no microserviço de internação

--- internacao/src/main/java/br/com/basis/madre/domain/evento/EventoPaciente.java
+++ internacao/src/main/java/br/com/basis/madre/domain/evento/EventoPaciente.java
@@ -1,11 +1,17 @@
 package br.com.basis.madre.domain.evento;
 
 import br.com.basis.madre.domain.Paciente;
+import br.com.basis.madre.domain.enumeration.TipoEvento;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 
 @Data
+@Builder
 public class EventoPaciente implements Serializable {
-    private Paciente paciente;
+    private final Paciente paciente;
+    private final ZonedDateTime dataDeLancamento;
+    private final TipoEvento tipoDoEvento;
 }
