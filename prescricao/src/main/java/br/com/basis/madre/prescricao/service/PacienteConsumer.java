55ab416d84887c56b32a66cee4d7b3025f60cda1
Mateus Braga <mateus.braga@basis.com.br>
2020-06-03 18:31:50 -0300

    Ajustes no código - BASIS-128436

--- prescricao/src/main/java/br/com/basis/madre/prescricao/service/PacienteConsumer.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/service/PacienteConsumer.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import br.com.basis.madre.prescricao.domain.Paciente;
+import br.com.basis.madre.prescricao.domain.evento.EventoPaciente;
 import br.com.basis.madre.prescricao.repository.search.PacienteRepositorySearch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +15,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class PacienteConsumer implements Consumer<Message<Paciente>> {
-	private PacienteRepositorySearch pacienteRepositorySearch;
+public class PacienteConsumer implements Consumer<Message<EventoPaciente>> {
+	private final PacienteRepositorySearch pacienteRepositorySearch;
 
 	@Override
-	public void accept(Message<Paciente> pacienteMessage) {
-		// TODO Auto-generated method stub
-		log.debug("Mensagem");
-		pacienteRepositorySearch.save(pacienteMessage.getPayload());
+	public void accept(Message<EventoPaciente> pacienteMessage) {
+		Paciente paciente = pacienteMessage.getPayload().getPaciente();
+		log.debug("Mensagem recebida = {}", paciente);
+		pacienteRepositorySearch.save(paciente);
 		
 	}
+
 }
