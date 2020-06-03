7a041b86680c6b39b283054848c03c56bef41063
Mateus Braga <mateus.braga@basis.com.br>
2020-06-03 18:27:28 -0300

    Ajuste na classe de evento de paciente - BASIS-128436

--- prescricao/src/main/java/br/com/basis/madre/prescricao/domain/evento/EventoPaciente.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/domain/evento/EventoPaciente.java
@@ -3,16 +3,13 @@ package br.com.basis.madre.prescricao.domain.evento;
 import java.io.Serializable;
 
 import br.com.basis.madre.prescricao.domain.Paciente;
-import lombok.Builder;
 import lombok.Data;
-
 @Data
-@Builder
 public class EventoPaciente implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private final Paciente paciente;
+	private Paciente paciente;
 
 }
