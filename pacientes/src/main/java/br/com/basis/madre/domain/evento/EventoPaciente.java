b63417a2ed49aa2d6579776dc22364d8e0b6fbbe
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-06-03 11:06:35 -0300

    Remove unused imports

--- pacientes/src/main/java/br/com/basis/madre/domain/evento/EventoPaciente.java
+++ pacientes/src/main/java/br/com/basis/madre/domain/evento/EventoPaciente.java
@@ -1,11 +1,8 @@
 package br.com.basis.madre.domain.evento;
 
 import br.com.basis.madre.domain.Paciente;
-import br.com.basis.madre.domain.Triagem;
 import br.com.basis.madre.domain.enumeration.TipoDeMutacao;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.ZoneId;
