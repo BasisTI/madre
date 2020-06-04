5737cb8972be6cc1cb75fae92235579a81fc4c0b
Mateus Braga <mateus.braga@basis.com.br>
2020-06-04 12:55:35 -0300

    Adiciona campos solicitados -  BASIS-128436

--- prescricao/src/main/java/br/com/basis/madre/prescricao/domain/evento/EventoPrescricaoMedicamento.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/domain/evento/EventoPrescricaoMedicamento.java
@@ -1,8 +1,10 @@
 package br.com.basis.madre.prescricao.domain.evento;
 
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 
 import br.com.basis.madre.prescricao.domain.PrescricaoMedicamento;
+import br.com.basis.madre.prescricao.domain.enumeration.TipoEvento;
 import lombok.Data;
 
 @Data
@@ -10,6 +12,9 @@ public class EventoPrescricaoMedicamento implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	private PrescricaoMedicamento prescricaoMedicamento;
+	private ZonedDateTime dataDeLancamento;
+	private TipoEvento tipoDoEvento;
+	private String login;
 	
 
 }
