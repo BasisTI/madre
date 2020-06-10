2ad0288f39029e9f8a370e49dbdd5dbad16f42bf
Mateus Braga <mateus.braga@basis.com.br>
2020-06-10 20:08:51 -0300

    Removendo endpoint desnecessário -  BASIS-130576

--- prescricao/src/main/java/br/com/basis/madre/prescricao/web/rest/PacienteResource.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/web/rest/PacienteResource.java
@@ -7,18 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,12 +33,6 @@ public class PacienteResource {
 
     @Autowired
     private PacienteService pacienteService;
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void criarPaciente(Paciente paciente) {
-        pacienteRepositorySearch.save(paciente);
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Paciente> buscarPorId(@PathVariable Long id) {
