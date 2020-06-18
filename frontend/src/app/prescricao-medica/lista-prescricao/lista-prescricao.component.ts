38ba3d206ef5c6a81725b8bbd9a5603e00074d02
Mateus Braga <mateus.braga@basis.com.br>
2020-06-18 19:06:43 -0300

    Criando componente lista de prescrições - BASIS-131379

---
+++ frontend/src/app/prescricao-medica/lista-prescricao/lista-prescricao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-prescricao',
+  templateUrl: './lista-prescricao.component.html',
+  styleUrls: ['./lista-prescricao.component.css']
+})
+export class ListaPrescricaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
