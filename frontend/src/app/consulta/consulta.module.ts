dda767e697c0b5dd284360e9abbc1ed2d2c90611
Jocimar Neres <jocimar.silva@basis.com.br>
2020-06-19 14:21:35 -0300

    Exlui imports sem uso em consulta-routes e modules

--- frontend/src/app/consulta/consulta.module.ts
+++ frontend/src/app/consulta/consulta.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { routes } from './consulta.routes';
-import { EmergenciaComponent } from './components/emergencia/emergencia.component';
+
 import { ListarConsultasComponent } from './components/listar-consultas/listar-consultas.component';
 
 @NgModule({
-    declarations: [EmergenciaComponent, ListarConsultasComponent],
+    declarations: [ListarConsultasComponent],
     imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
 })
 export class ConsultaModule {}
