81ee17f799fbd2a023e1ccad480c7db7b31a6c03
Mateus Braga <mateus.braga@basis.com.br>
2020-06-19 20:17:36 -0300

    removendo campos e adicionando fetchtype - BASIS-131379

--- prescricao/src/main/java/br/com/basis/madre/prescricao/domain/PrescricaoDieta.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/domain/PrescricaoDieta.java
@@ -1,31 +1,24 @@
 package br.com.basis.madre.prescricao.domain;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import net.logstash.logback.encoder.org.apache.commons.lang3.builder.ToStringExclude;
 
 /**
  * A PrescricaoDieta.
@@ -41,30 +34,15 @@ public class PrescricaoDieta extends PrescricaoMedica implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Column(name = "id_paciente")
-	private Long idPaciente;
-
 	@Column(name = "bomba_infusao")
 	private Boolean bombaInfusao;
 
-	@Size(max = 255)
-	@Column(name = "observacao", length = 255)
-	private String observacao;
 
-	@EqualsAndHashCode.Exclude
+	@ToStringExclude
 	@OneToMany(mappedBy = "prescricaoDieta", cascade = CascadeType.ALL)
 	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private Set<ItemPrescricaoDieta> itemPrescricaoDietas = new HashSet<>();
 
-	public PrescricaoDieta idPaciente(Long idPaciente) {
-		this.idPaciente = idPaciente;
-		return this;
-	}
-
-	public PrescricaoDieta observacao(String observacao) {
-		this.observacao = observacao;
-		return this;
-	}
 
 	public PrescricaoDieta itemPrescricaoDietas(Set<ItemPrescricaoDieta> itemPrescricaoDietas) {
 		this.itemPrescricaoDietas = itemPrescricaoDietas;
--- prescricao/src/main/java/br/com/basis/madre/prescricao/domain/PrescricaoMedicamento.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/domain/PrescricaoMedicamento.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
@@ -35,27 +36,10 @@ public class PrescricaoMedicamento extends PrescricaoMedica implements Serializa
 
 	private static final long serialVersionUID = 1L;
 
-	@Column(name = "id_paciente")
-	private Long idPaciente;
-
-	@Size(max = 255)
-	@Column(name = "observacao", length = 255)
-	private String observacao;
-
 	@EqualsAndHashCode.Exclude
-	@OneToMany(mappedBy = "prescricaoMedicamento", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "prescricaoMedicamento", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private Set<ItemPrescricaoMedicamento> itemPrescricaoMedicamentos = new HashSet<>();
-
-	public PrescricaoMedicamento idPaciente(Long idPaciente) {
-		this.idPaciente = idPaciente;
-		return this;
-	}
-
-	public PrescricaoMedicamento observacao(String observacao) {
-		this.observacao = observacao;
-		return this;
-	}
 
 	public PrescricaoMedicamento itemPrescricaoMedicamentos(Set<ItemPrescricaoMedicamento> itemPrescricaoMedicamentos) {
 		this.itemPrescricaoMedicamentos = itemPrescricaoMedicamentos;
