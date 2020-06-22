467d7ab5d9c5505749c8b7ae6593d0db82c4545e
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-06-22 14:53:31 -0300

    Adiciona anotação para validar CPF e CNPJ no mesmo campo

---
+++ suprimentos/src/test/java/br/com/basis/suprimentos/domain/validator/CpfCnpjValidatorTest.java
@@ -0,0 +1,69 @@
+package br.com.basis.suprimentos.domain.validator;
+
+import br.com.basis.suprimentos.domain.annotation.CpfCnpj;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+class CpfCnpjValidatorTest {
+
+    Validator validator;
+    final ExpectedException exception = ExpectedException.none();
+
+    @BeforeEach
+    void init() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    void cpfValidoTest() {
+        final CpfPlaceholder placeholder = new CpfPlaceholder("04175516140");
+        final Set<ConstraintViolation<CpfPlaceholder>> constraintViolations = validator.validate(placeholder);
+        assertTrue(constraintViolations.isEmpty());
+    }
+
+    @Test
+    void cpfInvalidoTest() {
+        final CpfPlaceholder placeholder = new CpfPlaceholder("04175516141");
+        final Set<ConstraintViolation<CpfPlaceholder>> constraintViolations = validator.validate(placeholder);
+        assertFalse(constraintViolations.isEmpty());
+    }
+
+    @Test
+    void cnpjValidoTest() {
+        final CnpjPlaceholder placeholder = new CnpjPlaceholder("71221524000150");
+        final Set<ConstraintViolation<CnpjPlaceholder>> constraintViolations = validator.validate(placeholder);
+        assertTrue(constraintViolations.isEmpty());
+    }
+
+    @Test
+    void cnpjInvalidoTest() {
+        final CnpjPlaceholder placeholder = new CnpjPlaceholder("71021524000150");
+        final Set<ConstraintViolation<CnpjPlaceholder>> constraintViolations = validator.validate(placeholder);
+        assertFalse(constraintViolations.isEmpty());
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    public static class CpfPlaceholder {
+        @CpfCnpj
+        private final String cpf;
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    public static class CnpjPlaceholder {
+        @CpfCnpj
+        private final String cnpj;
+    }
+}
