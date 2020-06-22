346792fa088d069c377d5668006f3b08299f8bd9
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-06-22 11:11:51 -0300

    Ajustes nos imports do código + adicina lombok para manter a qualidade

--- suprimentos/src/main/java/br/com/basis/suprimentos/domain/Material.java
+++ suprimentos/src/main/java/br/com/basis/suprimentos/domain/Material.java
@@ -1,8 +1,11 @@
 package br.com.basis.suprimentos.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import javax.persistence.Column;
@@ -18,21 +21,18 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
-/**
- * A Material.
- */
+@Data
 @Entity
 @Table(name = "material")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-@org.springframework.data.elasticsearch.annotations.Document(indexName = "material")
+@Document(indexName = "madre-suprimentos-material")
 public class Material implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     @SequenceGenerator(name = "sequenceGenerator")
-    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword)
     private Long id;
 
     @NotNull
@@ -94,226 +94,4 @@ public class Material implements Serializable {
     @ManyToOne
     @JsonIgnoreProperties("materials")
     private OrigemParecerTecnico origemParecerTecnico;
-
-    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Material nome(String nome) {
-        this.nome = nome;
-        return this;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Material descricao(String descricao) {
-        this.descricao = descricao;
-        return this;
-    }
-
-    public Boolean isAtivo() {
-        return ativo;
-    }
-
-    public Material ativo(Boolean ativo) {
-        this.ativo = ativo;
-        return this;
-    }
-
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
-    }
-
-    public String getRegimento() {
-        return regimento;
-    }
-
-    public void setRegimento(String regimento) {
-        this.regimento = regimento;
-    }
-
-    public Material regimento(String regimento) {
-        this.regimento = regimento;
-        return this;
-    }
-
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-
-    public Material observacao(String observacao) {
-        this.observacao = observacao;
-        return this;
-    }
-
-    public Long getUnidadeId() {
-        return unidadeId;
-    }
-
-    public void setUnidadeId(Long unidadeId) {
-        this.unidadeId = unidadeId;
-    }
-
-    public Material unidadeId(Long unidadeId) {
-        this.unidadeId = unidadeId;
-        return this;
-    }
-
-    public Long getProcedimentoId() {
-        return procedimentoId;
-    }
-
-    public void setProcedimentoId(Long procedimentoId) {
-        this.procedimentoId = procedimentoId;
-    }
-
-    public Material procedimentoId(Long procedimentoId) {
-        this.procedimentoId = procedimentoId;
-        return this;
-    }
-
-    public UnidadeMedida getUnidadeMedida() {
-        return unidadeMedida;
-    }
-
-    public void setUnidadeMedida(UnidadeMedida unidadeMedida) {
-        this.unidadeMedida = unidadeMedida;
-    }
-
-    public Material unidadeMedida(UnidadeMedida unidadeMedida) {
-        this.unidadeMedida = unidadeMedida;
-        return this;
-    }
-
-    public GrupoMaterial getGrupo() {
-        return grupo;
-    }
-
-    public void setGrupo(GrupoMaterial grupoMaterial) {
-        this.grupo = grupoMaterial;
-    }
-
-    public Material grupo(GrupoMaterial grupoMaterial) {
-        this.grupo = grupoMaterial;
-        return this;
-    }
-
-    public Almoxarifado getLocalEstoque() {
-        return localEstoque;
-    }
-
-    public void setLocalEstoque(Almoxarifado almoxarifado) {
-        this.localEstoque = almoxarifado;
-    }
-
-    public Material localEstoque(Almoxarifado almoxarifado) {
-        this.localEstoque = almoxarifado;
-        return this;
-    }
-
-    public CodigoCatmat getCodigoCatmat() {
-        return codigoCatmat;
-    }
-
-    public void setCodigoCatmat(CodigoCatmat codigoCatmat) {
-        this.codigoCatmat = codigoCatmat;
-    }
-
-    public Material codigoCatmat(CodigoCatmat codigoCatmat) {
-        this.codigoCatmat = codigoCatmat;
-        return this;
-    }
-
-    public Sazonalidade getSazonalidade() {
-        return sazonalidade;
-    }
-
-    public void setSazonalidade(Sazonalidade sazonalidade) {
-        this.sazonalidade = sazonalidade;
-    }
-
-    public Material sazonalidade(Sazonalidade sazonalidade) {
-        this.sazonalidade = sazonalidade;
-        return this;
-    }
-
-    public TipoResiduo getTipoResiduo() {
-        return tipoResiduo;
-    }
-
-    public void setTipoResiduo(TipoResiduo tipoResiduo) {
-        this.tipoResiduo = tipoResiduo;
-    }
-
-    public Material tipoResiduo(TipoResiduo tipoResiduo) {
-        this.tipoResiduo = tipoResiduo;
-        return this;
-    }
-
-    public OrigemParecerTecnico getOrigemParecerTecnico() {
-        return origemParecerTecnico;
-    }
-
-    public void setOrigemParecerTecnico(OrigemParecerTecnico origemParecerTecnico) {
-        this.origemParecerTecnico = origemParecerTecnico;
-    }
-
-    public Material origemParecerTecnico(OrigemParecerTecnico origemParecerTecnico) {
-        this.origemParecerTecnico = origemParecerTecnico;
-        return this;
-    }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Material)) {
-            return false;
-        }
-        return id != null && id.equals(((Material) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        return 31;
-    }
-
-    @Override
-    public String toString() {
-        return "Material{" +
-            "id=" + getId() +
-            ", nome='" + getNome() + "'" +
-            ", descricao='" + getDescricao() + "'" +
-            ", ativo='" + isAtivo() + "'" +
-            ", regimento='" + getRegimento() + "'" +
-            ", observacao='" + getObservacao() + "'" +
-            ", unidadeId=" + getUnidadeId() +
-            ", procedimentoId=" + getProcedimentoId() +
-            "}";
-    }
 }
