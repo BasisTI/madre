0b11ce3b61388c0b5b895c39ef1382b468f6dbfa
Pedro Henrique Fernandes <pedro.fernandes@basis.com.br>
2020-06-30 11:12:47 -0300

    Retira CPF/CNPJ da NF + Adiciona CPF/CNPJ ao Fornecedor

--- suprimentos/src/main/java/br/com/basis/suprimentos/domain/DocumentoFiscalEntrada.java
+++ suprimentos/src/main/java/br/com/basis/suprimentos/domain/DocumentoFiscalEntrada.java
@@ -1,6 +1,5 @@
 package br.com.basis.suprimentos.domain;
 
-import br.com.basis.suprimentos.domain.annotation.CpfCnpj;
 import br.com.basis.suprimentos.domain.enumeration.TipoDocumento;
 import br.com.basis.suprimentos.domain.enumeration.TipoDocumentoFiscal;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -53,12 +52,6 @@ public class DocumentoFiscalEntrada implements Serializable {
     @Size(max = 120)
     @Column(name = "nota_empenho", length = 120)
     private String notaEmpenho;
-
-    @CpfCnpj
-    @NotNull
-    @Size(min = 11, max = 14)
-    @Column(name = "cpf_cnpj", length = 14, nullable = false, unique = true)
-    private String cpfCnpj;
 
     @NotNull
     @Column(name = "data_geracao", nullable = false)
