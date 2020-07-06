8d0b75e8ac89672e5d30f0b6b4713319cc9ea10b
Mateus Braga <mateus.braga@basis.com.br>
2020-07-06 16:58:47 -0300

    Ajustando codigo e listando prescrições por paciente - BASIS-131379

--- prescricao/src/main/java/br/com/basis/madre/prescricao/service/PrescricaoMedicaService.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/service/PrescricaoMedicaService.java
@@ -2,8 +2,11 @@ package br.com.basis.madre.prescricao.service;
 
 import br.com.basis.madre.prescricao.domain.PrescricaoMedica;
 import br.com.basis.madre.prescricao.repository.PrescricaoMedicaRepository;
+import br.com.basis.madre.prescricao.repository.search.PrescricaoMedicaDTOSearchRepository;
 import br.com.basis.madre.prescricao.repository.search.PrescricaoMedicaSearchRepository;
+import br.com.basis.madre.prescricao.service.dto.PrescricaoDietaDTO;
 import br.com.basis.madre.prescricao.service.dto.PrescricaoMedicaDTO;
+import br.com.basis.madre.prescricao.service.dto.PrescricaoMedicamentoDTO;
 import br.com.basis.madre.prescricao.service.mapper.PrescricaoMedicaMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
@@ -24,84 +28,98 @@ import static org.elasticsearch.index.query.QueryBuilders.*;
 @Transactional
 public class PrescricaoMedicaService {
 
-    private final Logger log = LoggerFactory.getLogger(PrescricaoMedicaService.class);
+	private final Logger log = LoggerFactory.getLogger(PrescricaoMedicaService.class);
 
-    private final PrescricaoMedicaRepository prescricaoMedicaRepository;
+	private final PrescricaoMedicaRepository prescricaoMedicaRepository;
 
-    private final PrescricaoMedicaMapper prescricaoMedicaMapper;
+	private final PrescricaoMedicaMapper prescricaoMedicaMapper;
 
-    private final PrescricaoMedicaSearchRepository prescricaoMedicaSearchRepository;
+	private final PrescricaoMedicaSearchRepository prescricaoMedicaSearchRepository;
 
-    public PrescricaoMedicaService(PrescricaoMedicaRepository prescricaoMedicaRepository, PrescricaoMedicaMapper prescricaoMedicaMapper, PrescricaoMedicaSearchRepository prescricaoMedicaSearchRepository) {
-        this.prescricaoMedicaRepository = prescricaoMedicaRepository;
-        this.prescricaoMedicaMapper = prescricaoMedicaMapper;
-        this.prescricaoMedicaSearchRepository = prescricaoMedicaSearchRepository;
-    }
+	private final PrescricaoMedicaDTOSearchRepository prescricaoMedicaDTOSearchRepository;
+	
 
-    /**
-     * Save a prescricaoMedica.
-     *
-     * @param prescricaoMedicaDTO the entity to save.
-     * @return the persisted entity.
-     */
-    public PrescricaoMedicaDTO save(PrescricaoMedicaDTO prescricaoMedicaDTO) {
-        log.debug("Request to save PrescricaoMedica : {}", prescricaoMedicaDTO);
-        PrescricaoMedica prescricaoMedica = prescricaoMedicaMapper.toEntity(prescricaoMedicaDTO);
-        prescricaoMedica = prescricaoMedicaRepository.save(prescricaoMedica);
-        PrescricaoMedicaDTO result = prescricaoMedicaMapper.toDto(prescricaoMedica);
-        prescricaoMedicaSearchRepository.save(prescricaoMedica);
-        return result;
-    }
+	public PrescricaoMedicaService(PrescricaoMedicaRepository prescricaoMedicaRepository,
+			PrescricaoMedicaMapper prescricaoMedicaMapper,
+			PrescricaoMedicaSearchRepository prescricaoMedicaSearchRepository,
+			PrescricaoMedicaDTOSearchRepository prescricaoMedicaDTOSearchRepository) {
+		this.prescricaoMedicaRepository = prescricaoMedicaRepository;
+		this.prescricaoMedicaMapper = prescricaoMedicaMapper;
+		this.prescricaoMedicaSearchRepository = prescricaoMedicaSearchRepository;
+		this.prescricaoMedicaDTOSearchRepository = prescricaoMedicaDTOSearchRepository;
+	}
 
-    /**
-     * Get all the prescricaoMedicas.
-     *
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    @Transactional(readOnly = true)
-    public Page<PrescricaoMedicaDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all PrescricaoMedicas");
-        return prescricaoMedicaRepository.findAll(pageable)
-            .map(prescricaoMedicaMapper::toDto);
-    }
+	/**
+	 * Save a prescricaoMedica.
+	 *
+	 * @param prescricaoMedicaDTO the entity to save.
+	 * @return the persisted entity.
+	 */
+	public PrescricaoMedicaDTO save(PrescricaoMedicaDTO prescricaoMedicaDTO) {
+		log.debug("Request to save PrescricaoMedica : {}", prescricaoMedicaDTO);
+		PrescricaoMedica prescricaoMedica = prescricaoMedicaMapper.toEntity(prescricaoMedicaDTO);
+		
+//		if(prescricaoMedicaDTO instanceof PrescricaoMedicamentoDTO) {
+//		}else if(prescricaoMedicaDTO instanceof PrescricaoDietaDTO) {
+//			
+//		} 
+		
+		prescricaoMedica = prescricaoMedicaRepository.save(prescricaoMedica);
+		PrescricaoMedicaDTO result = prescricaoMedicaMapper.toDto(prescricaoMedica);
+		prescricaoMedicaSearchRepository.save(prescricaoMedica);
+		return result;
+	}
 
+	/**
+	 * Get all the prescricaoMedicas.
+	 *
+	 * @param pageable the pagination information.
+	 * @return the list of entities.
+	 */
+	@Transactional(readOnly = true)
+	public Page<PrescricaoMedicaDTO> findAll(Pageable pageable) {
+		log.debug("Request to get all PrescricaoMedicas");
+		return prescricaoMedicaRepository.findAll(pageable).map(prescricaoMedicaMapper::toDto);
+	}
 
-    /**
-     * Get one prescricaoMedica by id.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    @Transactional(readOnly = true)
-    public Optional<PrescricaoMedicaDTO> findOne(Long id) {
-        log.debug("Request to get PrescricaoMedica : {}", id);
-        return prescricaoMedicaRepository.findById(id)
-            .map(prescricaoMedicaMapper::toDto);
-    }
+	/**
+	 * Get one prescricaoMedica by id.
+	 *
+	 * @param id the id of the entity.
+	 * @return the entity.
+	 */
+	@Transactional(readOnly = true)
+	public Optional<PrescricaoMedicaDTO> findOne(Long id) {
+		log.debug("Request to get PrescricaoMedica : {}", id);
+		return prescricaoMedicaRepository.findById(id).map(prescricaoMedicaMapper::toDto);
+	}
 
-    /**
-     * Delete the prescricaoMedica by id.
-     *
-     * @param id the id of the entity.
-     */
-    public void delete(Long id) {
-        log.debug("Request to delete PrescricaoMedica : {}", id);
-        prescricaoMedicaRepository.deleteById(id);
-        prescricaoMedicaSearchRepository.deleteById(id);
-    }
+	/**
+	 * Delete the prescricaoMedica by id.
+	 *
+	 * @param id the id of the entity.
+	 */
+	public void delete(Long id) {
+		log.debug("Request to delete PrescricaoMedica : {}", id);
+		prescricaoMedicaRepository.deleteById(id);
+		prescricaoMedicaSearchRepository.deleteById(id);
+	}
 
-    /**
-     * Search for the prescricaoMedica corresponding to the query.
-     *
-     * @param query the query of the search.
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    @Transactional(readOnly = true)
-    public Page<PrescricaoMedicaDTO> search(String query, Pageable pageable) {
-        log.debug("Request to search for a page of PrescricaoMedicas for query {}", query);
-        return prescricaoMedicaSearchRepository.search(queryStringQuery(query), pageable)
-            .map(prescricaoMedicaMapper::toDto);
-    }
+	/**
+	 * Search for the prescricaoMedica corresponding to the query.
+	 *
+	 * @param query    the query of the search.
+	 * @param pageable the pagination information.
+	 * @return the list of entities.
+	 */
+	@Transactional(readOnly = true)
+	public Page<PrescricaoMedicaDTO> search(String query, Pageable pageable) {
+		log.debug("Request to search for a page of PrescricaoMedicas for query {}", query);
+		return prescricaoMedicaSearchRepository.search(queryStringQuery(query), pageable)
+				.map(prescricaoMedicaMapper::toDto);
+	}
+
+	public List<PrescricaoMedicaDTO> searchByIdPaciente(Long id) {
+		return prescricaoMedicaDTOSearchRepository.findByIdPaciente(id);
+	}
 }
--- prescricao/src/main/java/br/com/basis/madre/prescricao/web/rest/PrescricaoMedicaResource.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/web/rest/PrescricaoMedicaResource.java
@@ -27,136 +27,149 @@ import br.com.basis.madre.prescricao.domain.PrescricaoMedica;
 import br.com.basis.madre.prescricao.repository.PrescricaoMedicaRepository;
 import br.com.basis.madre.prescricao.service.PrescricaoMedicaService;
 import br.com.basis.madre.prescricao.service.dto.PrescricaoMedicaDTO;
+import br.com.basis.madre.prescricao.service.dto.PrescricaoMedicamentoDTO;
 import br.gov.nuvem.comum.microsservico.web.rest.errors.BadRequestAlertException;
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.PaginationUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 
 /**
- * REST controller for managing {@link br.com.basis.madre.prescricao.domain.PrescricaoMedica}.
+ * REST controller for managing
+ * {@link br.com.basis.madre.prescricao.domain.PrescricaoMedica}.
  */
 @RestController
 @RequestMapping("/api")
 public class PrescricaoMedicaResource {
 
-    private final Logger log = LoggerFactory.getLogger(PrescricaoMedicaResource.class);
+	private final Logger log = LoggerFactory.getLogger(PrescricaoMedicaResource.class);
 
-    private static final String ENTITY_NAME = "prescricaoPrescricaoMedica";
+	private static final String ENTITY_NAME = "prescricaoPrescricaoMedica";
 
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
+	@Value("${jhipster.clientApp.name}")
+	private String applicationName;
 
-    private final PrescricaoMedicaService prescricaoMedicaService;
-    
-    private final PrescricaoMedicaRepository prescricaoMedicaRepository;
+	private final PrescricaoMedicaService prescricaoMedicaService;
 
-    public PrescricaoMedicaResource(PrescricaoMedicaService prescricaoMedicaService, PrescricaoMedicaRepository prescricaoMedicaRepository) {
-        this.prescricaoMedicaService = prescricaoMedicaService;
-        this.prescricaoMedicaRepository = prescricaoMedicaRepository;
-    }
+	public PrescricaoMedicaResource(PrescricaoMedicaService prescricaoMedicaService) {
+		this.prescricaoMedicaService = prescricaoMedicaService;
+	}
 
-    /**
-     * {@code POST  /prescricao-medicas} : Create a new prescricaoMedica.
-     *
-     * @param prescricaoMedicaDTO the prescricaoMedicaDTO to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new prescricaoMedicaDTO, or with status {@code 400 (Bad Request)} if the prescricaoMedica has already an ID.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PostMapping("/prescricao-medicas")
-    public ResponseEntity<PrescricaoMedicaDTO> createPrescricaoMedica(@RequestBody PrescricaoMedicaDTO prescricaoMedicaDTO) throws URISyntaxException {
-        log.debug("REST request to save PrescricaoMedica : {}", prescricaoMedicaDTO);
-        if (prescricaoMedicaDTO.getId() != null) {
-            throw new BadRequestAlertException("A new prescricaoMedica cannot already have an ID", ENTITY_NAME, "idexists");
-        }
-        PrescricaoMedicaDTO result = prescricaoMedicaService.save(prescricaoMedicaDTO);
-        return ResponseEntity.created(new URI("/api/prescricao-medicas/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
+	/**
+	 * {@code POST  /prescricao-medicas} : Create a new prescricaoMedica.
+	 *
+	 * @param prescricaoMedicaDTO the prescricaoMedicaDTO to create.
+	 * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with
+	 *         body the new prescricaoMedicaDTO, or with status
+	 *         {@code 400 (Bad Request)} if the prescricaoMedica has already an ID.
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 */
+	@PostMapping("/prescricao-medicas")
+	public ResponseEntity<PrescricaoMedicaDTO> createPrescricaoMedica(
+			@RequestBody PrescricaoMedicaDTO prescricaoMedicaDTO) throws URISyntaxException {
+		log.debug("REST request to save PrescricaoMedica : {}", prescricaoMedicaDTO);
+		if (prescricaoMedicaDTO.getId() != null) {
+			throw new BadRequestAlertException("A new prescricaoMedica cannot already have an ID", ENTITY_NAME,
+					"idexists");
+		}
+		PrescricaoMedicaDTO result = prescricaoMedicaService.save(prescricaoMedicaDTO);
+		return ResponseEntity
+				.created(new URI("/api/prescricao-medicas/" + result.getId())).headers(HeaderUtil
+						.createEntityCreationAlert(applicationName, false, ENTITY_NAME, result.getId().toString()))
+				.body(result);
+	}
 
-    /**
-     * {@code PUT  /prescricao-medicas} : Updates an existing prescricaoMedica.
-     *
-     * @param prescricaoMedicaDTO the prescricaoMedicaDTO to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated prescricaoMedicaDTO,
-     * or with status {@code 400 (Bad Request)} if the prescricaoMedicaDTO is not valid,
-     * or with status {@code 500 (Internal Server Error)} if the prescricaoMedicaDTO couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PutMapping("/prescricao-medicas")
-    public ResponseEntity<PrescricaoMedicaDTO> updatePrescricaoMedica(@RequestBody PrescricaoMedicaDTO prescricaoMedicaDTO) throws URISyntaxException {
-        log.debug("REST request to update PrescricaoMedica : {}", prescricaoMedicaDTO);
-        if (prescricaoMedicaDTO.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        PrescricaoMedicaDTO result = prescricaoMedicaService.save(prescricaoMedicaDTO);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME, prescricaoMedicaDTO.getId().toString()))
-            .body(result);
-    }
+	/**
+	 * {@code PUT  /prescricao-medicas} : Updates an existing prescricaoMedica.
+	 *
+	 * @param prescricaoMedicaDTO the prescricaoMedicaDTO to update.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body
+	 *         the updated prescricaoMedicaDTO, or with status
+	 *         {@code 400 (Bad Request)} if the prescricaoMedicaDTO is not valid, or
+	 *         with status {@code 500 (Internal Server Error)} if the
+	 *         prescricaoMedicaDTO couldn't be updated.
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 */
+	@PutMapping("/prescricao-medicas")
+	public ResponseEntity<PrescricaoMedicaDTO> updatePrescricaoMedica(
+			@RequestBody PrescricaoMedicaDTO prescricaoMedicaDTO) throws URISyntaxException {
+		log.debug("REST request to update PrescricaoMedica : {}", prescricaoMedicaDTO);
+		if (prescricaoMedicaDTO.getId() == null) {
+			throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+		}
+		PrescricaoMedicaDTO result = prescricaoMedicaService.save(prescricaoMedicaDTO);
+		return ResponseEntity.ok().headers(HeaderUtil.createEntityUpdateAlert(applicationName, false, ENTITY_NAME,
+				prescricaoMedicaDTO.getId().toString())).body(result);
+	}
 
-    /**
-     * {@code GET  /prescricao-medicas} : get all the prescricaoMedicas.
-     *
+	/**
+	 * {@code GET  /prescricao-medicas} : get all the prescricaoMedicas.
+	 *
+	 * 
+	 * @param pageable the pagination information.
+	 * 
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list
+	 *         of prescricaoMedicas in body.
+	 */
+	@GetMapping("/prescricao-medicas")
+	public ResponseEntity<List<PrescricaoMedicaDTO>> getAllPrescricaoMedicas(Pageable pageable) {
+		log.debug("REST request to get a page of PrescricaoMedicas");
+		Page<PrescricaoMedicaDTO> page = prescricaoMedicaService.findAll(pageable);
+		HttpHeaders headers = PaginationUtil
+				.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+		return ResponseEntity.ok().headers(headers).body(page.getContent());
+	}
 
-     * @param pageable the pagination information.
+	/**
+	 * {@code GET  /prescricao-medicas/:id} : get the "id" prescricaoMedica.
+	 *
+	 * @param id the id of the prescricaoMedicaDTO to retrieve.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body
+	 *         the prescricaoMedicaDTO, or with status {@code 404 (Not Found)}.
+	 */
+	@GetMapping("/prescricao-medicas/{id}")
+	public ResponseEntity<PrescricaoMedicaDTO> getPrescricaoMedica(@PathVariable Long id) {
+		log.debug("REST request to get PrescricaoMedica : {}", id);
+		Optional<PrescricaoMedicaDTO> prescricaoMedicaDTO = prescricaoMedicaService.findOne(id);
+		return ResponseUtil.wrapOrNotFound(prescricaoMedicaDTO);
+	}
 
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of prescricaoMedicas in body.
-     */
-    @GetMapping("/prescricao-medicas")
-    public ResponseEntity<List<PrescricaoMedicaDTO>> getAllPrescricaoMedicas(Pageable pageable) {
-        log.debug("REST request to get a page of PrescricaoMedicas");
-        Page<PrescricaoMedicaDTO> page = prescricaoMedicaService.findAll(pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
-        return ResponseEntity.ok().headers(headers).body(page.getContent());
-    }
+	/**
+	 * {@code DELETE  /prescricao-medicas/:id} : delete the "id" prescricaoMedica.
+	 *
+	 * @param id the id of the prescricaoMedicaDTO to delete.
+	 * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+	 */
+	@DeleteMapping("/prescricao-medicas/{id}")
+	public ResponseEntity<Void> deletePrescricaoMedica(@PathVariable Long id) {
+		log.debug("REST request to delete PrescricaoMedica : {}", id);
+		prescricaoMedicaService.delete(id);
+		return ResponseEntity.noContent()
+				.headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString()))
+				.build();
+	}
 
-    /**
-     * {@code GET  /prescricao-medicas/:id} : get the "id" prescricaoMedica.
-     *
-     * @param id the id of the prescricaoMedicaDTO to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the prescricaoMedicaDTO, or with status {@code 404 (Not Found)}.
-     */
-    @GetMapping("/prescricao-medicas/{id}")
-    public ResponseEntity<PrescricaoMedicaDTO> getPrescricaoMedica(@PathVariable Long id) {
-        log.debug("REST request to get PrescricaoMedica : {}", id);
-        Optional<PrescricaoMedicaDTO> prescricaoMedicaDTO = prescricaoMedicaService.findOne(id);
-        return ResponseUtil.wrapOrNotFound(prescricaoMedicaDTO);
-    }
+	/**
+	 * {@code SEARCH  /_search/prescricao-medicas?query=:query} : search for the
+	 * prescricaoMedica corresponding to the query.
+	 *
+	 * @param query    the query of the prescricaoMedica search.
+	 * @param pageable the pagination information.
+	 * @return the result of the search.
+	 */
+	@GetMapping("/_search/prescricao-medicas")
+	public ResponseEntity<List<PrescricaoMedicaDTO>> searchPrescricaoMedicas(@RequestParam String query,
+			Pageable pageable) {
+		log.debug("REST request to search for a page of PrescricaoMedicas for query {}", query);
+		Page<PrescricaoMedicaDTO> page = prescricaoMedicaService.search(query, pageable);
+		HttpHeaders headers = PaginationUtil
+				.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+		return ResponseEntity.ok().headers(headers).body(page.getContent());
+	}
 
-    /**
-     * {@code DELETE  /prescricao-medicas/:id} : delete the "id" prescricaoMedica.
-     *
-     * @param id the id of the prescricaoMedicaDTO to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-     */
-    @DeleteMapping("/prescricao-medicas/{id}")
-    public ResponseEntity<Void> deletePrescricaoMedica(@PathVariable Long id) {
-        log.debug("REST request to delete PrescricaoMedica : {}", id);
-        prescricaoMedicaService.delete(id);
-        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, false, ENTITY_NAME, id.toString())).build();
-    }
+	@GetMapping("/_search/prescricao-medicas/{id}")
+	public ResponseEntity<List<PrescricaoMedicaDTO>> searchByIdPaciente(@PathVariable Long id) {
+		List<PrescricaoMedicaDTO> prescricoes = prescricaoMedicaService.searchByIdPaciente(id);
 
-    /**
-     * {@code SEARCH  /_search/prescricao-medicas?query=:query} : search for the prescricaoMedica corresponding
-     * to the query.
-     *
-     * @param query the query of the prescricaoMedica search.
-     * @param pageable the pagination information.
-     * @return the result of the search.
-     */
-    @GetMapping("/_search/prescricao-medicas")
-    public ResponseEntity<List<PrescricaoMedicaDTO>> searchPrescricaoMedicas(@RequestParam String query, Pageable pageable) {
-        log.debug("REST request to search for a page of PrescricaoMedicas for query {}", query);
-        Page<PrescricaoMedicaDTO> page = prescricaoMedicaService.search(query, pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
-        return ResponseEntity.ok().headers(headers).body(page.getContent());
-    }
-    
-    @GetMapping("/prescricao-medicas/lista/{id}")
-    public ResponseEntity<List<PrescricaoMedica>> buscarPrescricoesPorPaciente(@PathVariable Long id){
-    	List<PrescricaoMedica> prescricoes = prescricaoMedicaRepository.findByIdPaciente(id);
-    	
-    	return ResponseEntity.ok(prescricoes);
-    }
+		return ResponseEntity.ok(prescricoes);
+	}
 }
