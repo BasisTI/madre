c7c22ed39be28294ffe537376f2a2b5a02067fef
Mateus Braga <mateus.braga@basis.com.br>
2020-07-13 17:53:16 -0300

    Criando tipo Diagnostico e salvando no ElasticSearch - BASIS-136408

--- prescricao/src/main/java/br/com/basis/madre/prescricao/service/PrescricaoDiagnosticoService.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/service/PrescricaoDiagnosticoService.java
@@ -1,21 +1,23 @@
 package br.com.basis.madre.prescricao.service;
 
-import br.com.basis.madre.prescricao.domain.PrescricaoDiagnostico;
-import br.com.basis.madre.prescricao.repository.PrescricaoDiagnosticoRepository;
-import br.com.basis.madre.prescricao.repository.search.PrescricaoDiagnosticoSearchRepository;
-import br.com.basis.madre.prescricao.service.dto.PrescricaoDiagnosticoDTO;
-import br.com.basis.madre.prescricao.service.mapper.PrescricaoDiagnosticoMapper;
+import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-import static org.elasticsearch.index.query.QueryBuilders.*;
+import br.com.basis.madre.prescricao.domain.ItemPrescricaoDiagnostico;
+import br.com.basis.madre.prescricao.domain.PrescricaoDiagnostico;
+import br.com.basis.madre.prescricao.repository.PrescricaoDiagnosticoRepository;
+import br.com.basis.madre.prescricao.repository.search.PrescricaoDiagnosticoDTOSearchRepository;
+import br.com.basis.madre.prescricao.repository.search.PrescricaoDiagnosticoSearchRepository;
+import br.com.basis.madre.prescricao.service.dto.PrescricaoDiagnosticoDTO;
+import br.com.basis.madre.prescricao.service.mapper.PrescricaoDiagnosticoMapper;
 
 /**
  * Service Implementation for managing {@link PrescricaoDiagnostico}.
@@ -24,84 +26,91 @@ import static org.elasticsearch.index.query.QueryBuilders.*;
 @Transactional
 public class PrescricaoDiagnosticoService {
 
-    private final Logger log = LoggerFactory.getLogger(PrescricaoDiagnosticoService.class);
+	private final Logger log = LoggerFactory.getLogger(PrescricaoDiagnosticoService.class);
 
-    private final PrescricaoDiagnosticoRepository prescricaoDiagnosticoRepository;
+	private final PrescricaoDiagnosticoRepository prescricaoDiagnosticoRepository;
 
-    private final PrescricaoDiagnosticoMapper prescricaoDiagnosticoMapper;
+	private final PrescricaoDiagnosticoMapper prescricaoDiagnosticoMapper;
 
-    private final PrescricaoDiagnosticoSearchRepository prescricaoDiagnosticoSearchRepository;
+	private final PrescricaoDiagnosticoSearchRepository prescricaoDiagnosticoSearchRepository;
 
-    public PrescricaoDiagnosticoService(PrescricaoDiagnosticoRepository prescricaoDiagnosticoRepository, PrescricaoDiagnosticoMapper prescricaoDiagnosticoMapper, PrescricaoDiagnosticoSearchRepository prescricaoDiagnosticoSearchRepository) {
-        this.prescricaoDiagnosticoRepository = prescricaoDiagnosticoRepository;
-        this.prescricaoDiagnosticoMapper = prescricaoDiagnosticoMapper;
-        this.prescricaoDiagnosticoSearchRepository = prescricaoDiagnosticoSearchRepository;
-    }
+	private final PrescricaoDiagnosticoDTOSearchRepository prescricaoDiagnosticoDTOSearchRepository;
 
-    /**
-     * Save a prescricaoDiagnostico.
-     *
-     * @param prescricaoDiagnosticoDTO the entity to save.
-     * @return the persisted entity.
-     */
-    public PrescricaoDiagnosticoDTO save(PrescricaoDiagnosticoDTO prescricaoDiagnosticoDTO) {
-        log.debug("Request to save PrescricaoDiagnostico : {}", prescricaoDiagnosticoDTO);
-        PrescricaoDiagnostico prescricaoDiagnostico = prescricaoDiagnosticoMapper.toEntity(prescricaoDiagnosticoDTO);
-        prescricaoDiagnostico = prescricaoDiagnosticoRepository.save(prescricaoDiagnostico);
-        PrescricaoDiagnosticoDTO result = prescricaoDiagnosticoMapper.toDto(prescricaoDiagnostico);
-        prescricaoDiagnosticoSearchRepository.save(prescricaoDiagnostico);
-        return result;
-    }
+	public PrescricaoDiagnosticoService(PrescricaoDiagnosticoRepository prescricaoDiagnosticoRepository,
+			PrescricaoDiagnosticoMapper prescricaoDiagnosticoMapper,
+			PrescricaoDiagnosticoSearchRepository prescricaoDiagnosticoSearchRepository,
+			PrescricaoDiagnosticoDTOSearchRepository prescricaoDiagnosticoDTOSearchRepository) {
+		this.prescricaoDiagnosticoRepository = prescricaoDiagnosticoRepository;
+		this.prescricaoDiagnosticoMapper = prescricaoDiagnosticoMapper;
+		this.prescricaoDiagnosticoSearchRepository = prescricaoDiagnosticoSearchRepository;
+		this.prescricaoDiagnosticoDTOSearchRepository = prescricaoDiagnosticoDTOSearchRepository;
+	}
 
-    /**
-     * Get all the prescricaoDiagnosticos.
-     *
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    @Transactional(readOnly = true)
-    public Page<PrescricaoDiagnosticoDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all PrescricaoDiagnosticos");
-        return prescricaoDiagnosticoRepository.findAll(pageable)
-            .map(prescricaoDiagnosticoMapper::toDto);
-    }
+	/**
+	 * Save a prescricaoDiagnostico.
+	 *
+	 * @param prescricaoDiagnosticoDTO the entity to save.
+	 * @return the persisted entity.
+	 */
+	public PrescricaoDiagnosticoDTO save(PrescricaoDiagnosticoDTO prescricaoDiagnosticoDTO) {
+		log.debug("Request to save PrescricaoDiagnostico : {}", prescricaoDiagnosticoDTO);
 
+		PrescricaoDiagnostico prescricaoDiagnostico = prescricaoDiagnosticoMapper.toEntity(prescricaoDiagnosticoDTO);
+		for (ItemPrescricaoDiagnostico item : prescricaoDiagnostico.getItemPrescricaoDiagnosticos()) {
+			item.setPrescricaoDiagnostico(prescricaoDiagnostico);
+		}
+		prescricaoDiagnostico = prescricaoDiagnosticoRepository.save(prescricaoDiagnostico);
+		PrescricaoDiagnosticoDTO result = prescricaoDiagnosticoMapper.toDto(prescricaoDiagnostico);
+		prescricaoDiagnosticoDTOSearchRepository.save(result);
+		return result;
+	}
 
-    /**
-     * Get one prescricaoDiagnostico by id.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    @Transactional(readOnly = true)
-    public Optional<PrescricaoDiagnosticoDTO> findOne(Long id) {
-        log.debug("Request to get PrescricaoDiagnostico : {}", id);
-        return prescricaoDiagnosticoRepository.findById(id)
-            .map(prescricaoDiagnosticoMapper::toDto);
-    }
+	/**
+	 * Get all the prescricaoDiagnosticos.
+	 *
+	 * @param pageable the pagination information.
+	 * @return the list of entities.
+	 */
+	@Transactional(readOnly = true)
+	public Page<PrescricaoDiagnosticoDTO> findAll(Pageable pageable) {
+		log.debug("Request to get all PrescricaoDiagnosticos");
+		return prescricaoDiagnosticoRepository.findAll(pageable).map(prescricaoDiagnosticoMapper::toDto);
+	}
 
-    /**
-     * Delete the prescricaoDiagnostico by id.
-     *
-     * @param id the id of the entity.
-     */
-    public void delete(Long id) {
-        log.debug("Request to delete PrescricaoDiagnostico : {}", id);
-        prescricaoDiagnosticoRepository.deleteById(id);
-        prescricaoDiagnosticoSearchRepository.deleteById(id);
-    }
+	/**
+	 * Get one prescricaoDiagnostico by id.
+	 *
+	 * @param id the id of the entity.
+	 * @return the entity.
+	 */
+	@Transactional(readOnly = true)
+	public Optional<PrescricaoDiagnosticoDTO> findOne(Long id) {
+		log.debug("Request to get PrescricaoDiagnostico : {}", id);
+		return prescricaoDiagnosticoRepository.findById(id).map(prescricaoDiagnosticoMapper::toDto);
+	}
 
-    /**
-     * Search for the prescricaoDiagnostico corresponding to the query.
-     *
-     * @param query the query of the search.
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    @Transactional(readOnly = true)
-    public Page<PrescricaoDiagnosticoDTO> search(String query, Pageable pageable) {
-        log.debug("Request to search for a page of PrescricaoDiagnosticos for query {}", query);
-        return prescricaoDiagnosticoSearchRepository.search(queryStringQuery(query), pageable)
-            .map(prescricaoDiagnosticoMapper::toDto);
-    }
+	/**
+	 * Delete the prescricaoDiagnostico by id.
+	 *
+	 * @param id the id of the entity.
+	 */
+	public void delete(Long id) {
+		log.debug("Request to delete PrescricaoDiagnostico : {}", id);
+		prescricaoDiagnosticoRepository.deleteById(id);
+		prescricaoDiagnosticoSearchRepository.deleteById(id);
+	}
+
+	/**
+	 * Search for the prescricaoDiagnostico corresponding to the query.
+	 *
+	 * @param query    the query of the search.
+	 * @param pageable the pagination information.
+	 * @return the list of entities.
+	 */
+	@Transactional(readOnly = true)
+	public Page<PrescricaoDiagnosticoDTO> search(String query, Pageable pageable) {
+		log.debug("Request to search for a page of PrescricaoDiagnosticos for query {}", query);
+		return prescricaoDiagnosticoSearchRepository.search(queryStringQuery(query), pageable)
+				.map(prescricaoDiagnosticoMapper::toDto);
+	}
 }
