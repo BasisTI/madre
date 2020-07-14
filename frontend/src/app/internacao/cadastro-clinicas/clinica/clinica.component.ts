82664a28134f32fdbea9416690fb107860b63a73
Vinicius <amorimhavoc@gmail.com>
2020-07-14 09:59:45 -0300

    correção

---
+++ frontend/src/app/internacao/cadastro-clinicas/clinica/clinica.component.ts
@@ -0,0 +1,26 @@
+import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clinica',
+  templateUrl: './clinica.component.html',
+  styleUrls: ['./clinica.component.css']
+})
+export class ClinicaComponent implements OnInit {
+
+    clinica = this.fb.group({
+    descricao: [null,Validators.required],
+    capacidadeReferencial: [null, Validators.required],
+    numeroSUS: [null],
+    idadeMinimaInternacao: [null,Validators.required],
+    idadeMaximaInternacao:  [null, Validators.required],
+    idadeMinimaAmbulatorio: [null, Validators.required],
+    idadeMaximaAmbulatorio: [null, Validators.required],
+    });
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+}
