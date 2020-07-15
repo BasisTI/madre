df396ff18ab1c65d8af0b30f6c13ef3789b1a5ed
Mateus Braga <mateus.braga@basis.com.br>
2020-07-15 10:43:00 -0300

    chama componente no módulo - BASIS-136408

--- frontend/src/app/prescricao-medica/prescricao-medica.module.ts
+++ frontend/src/app/prescricao-medica/prescricao-medica.module.ts
@@ -1,3 +1,5 @@
+import { InternacaoModule } from './../internacao/internacao.module';
+import { CidComponent } from '@internacao/components/cid/cid.component';
 import { medicamentoRoute } from './medicamento/medicamento.routes';
 import { PrescricaoMedicaDietaModule } from './dieta/prescricao-medica-dieta.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,12 +9,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SharedModule } from './../shared/shared.module';
 
 import { PrescricaoMedicaComponent } from './prescricao-medica.component';
-import { prescricaoMedica } from './prescricao-medica.router';
+import { prescricaoMedica } from './prescricao-medica.routes';
 import { PrescricaoMedicaService } from './prescricao-medica.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MedicamentoComponent } from './medicamento/medicamento.component';
 import { ListaPrescricaoComponent } from './lista-prescricao/lista-prescricao.component';
 import { ListaPrescricaoService } from './lista-prescricao/lista-prescricao.service';
+import { DiagnosticoComponent } from './diagnostico/diagnostico.component';
+import { DiagnosticoService } from './diagnostico/diagnostico.service';
 
 
 
@@ -29,8 +33,10 @@ import { ListaPrescricaoService } from './lista-prescricao/lista-prescricao.serv
         PrescricaoMedicaComponent,
         MedicamentoComponent,
         ListaPrescricaoComponent,
+        DiagnosticoComponent,
+        CidComponent
     ],
-    providers: [PrescricaoMedicaService, ListaPrescricaoService],
+    providers: [PrescricaoMedicaService, ListaPrescricaoService, DiagnosticoService],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
