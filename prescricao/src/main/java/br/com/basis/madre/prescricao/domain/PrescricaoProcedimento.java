2a5f6b929adfaa636eecff013c0ec55bc955f5bb
Mateus Braga <mateus.braga@basis.com.br>
2020-07-20 13:35:59 -0300

    Utiliza herança nas classes - BASIS-137410

--- prescricao/src/main/java/br/com/basis/madre/prescricao/domain/PrescricaoProcedimento.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/domain/PrescricaoProcedimento.java
@@ -5,22 +5,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -33,30 +24,10 @@ import lombok.ToString;
 @Entity
 @Table(name = "prescricao_procedimento")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-@Document(indexName = "madre-prescricao-ssssssssprescricaoprocedimento")
-public class PrescricaoProcedimento implements Serializable {
+@Document(indexName = "madre-prescricao-prescricaomedica", type = "prescricaomedica")
+public class PrescricaoProcedimento extends PrescricaoMedica implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-	@SequenceGenerator(name = "sequenceGenerator")
-	@Field(type = FieldType.Keyword)
-	private Long id;
-
-	/**
-	 * Identificador do paciente
-	 */
-	@NotNull
-	@Column(name = "id_paciente", nullable = false)
-	private Long idPaciente;
-
-	/**
-	 * Observação ou comentário para a prescrição de procedimento especial
-	 */
-	@Size(max = 255)
-	@Column(name = "observacao", length = 255)
-	private String observacao;
 
 	@EqualsAndHashCode.Exclude
 	@ToString.Exclude
@@ -64,15 +35,6 @@ public class PrescricaoProcedimento implements Serializable {
 	@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 	private Set<ItemPrescricaoProcedimento> itemPrescricaoProcedimentos = new HashSet<>();
 
-	public PrescricaoProcedimento idPaciente(Long idPaciente) {
-		this.idPaciente = idPaciente;
-		return this;
-	}
-
-	public PrescricaoProcedimento observacao(String observacao) {
-		this.observacao = observacao;
-		return this;
-	}
 
 	public Set<ItemPrescricaoProcedimento> getItemPrescricaoProcedimentos() {
 		return itemPrescricaoProcedimentos;
--- prescricao/src/main/java/br/com/basis/madre/prescricao/service/dto/PrescricaoProcedimentoDTO.java
+++ prescricao/src/main/java/br/com/basis/madre/prescricao/service/dto/PrescricaoProcedimentoDTO.java
@@ -1,37 +1,20 @@
 package br.com.basis.madre.prescricao.service.dto;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
- * A DTO for the {@link br.com.basis.madre.prescricao.domain.PrescricaoProcedimento} entity.
+ * A DTO for the
+ * {@link br.com.basis.madre.prescricao.domain.PrescricaoProcedimento} entity.
  */
 @Data
-public class PrescricaoProcedimentoDTO implements Serializable {
+public class PrescricaoProcedimentoDTO extends PrescricaoMedicaDTO implements Serializable {
 
-    private Long id;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Identificador do paciente
-     */
-    @NotNull
-    @ApiModelProperty(value = "Identificador do paciente", required = true)
-    private Long idPaciente;
-
-    /**
-     * Observação ou comentário para a prescrição de procedimento especial
-     */
-    @Size(max = 255)
-    @ApiModelProperty(value = "Observação ou comentário para a prescrição de procedimento especial")
-    private String observacao;
-    
-    private Set<ItemPrescricaoProcedimentoDTO> itemPrescricaoProcedimentoDTO = new HashSet<>();
-
+	private Set<ItemPrescricaoProcedimentoDTO> itemPrescricaoProcedimentoDTO = new HashSet<>();
 
 }
