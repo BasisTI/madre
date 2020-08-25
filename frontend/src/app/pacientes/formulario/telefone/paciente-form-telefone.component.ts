d7e1f12e54081b4502b6f6455c378dfbb7112fc4
Vinicius <amorimhavoc@gmail.com>
2020-08-25 09:53:15 -0300

    Adicionando opção de excluir na inclusão de telefone

--- frontend/src/app/pacientes/formulario/telefone/paciente-form-telefone.component.ts
+++ frontend/src/app/pacientes/formulario/telefone/paciente-form-telefone.component.ts
@@ -1,3 +1,4 @@
+import { DatatableClickEvent } from '@nuvem/primeng-components';
 import { Component, Input } from "@angular/core";
 import { Validators, FormArray, FormBuilder } from "@angular/forms";
 
@@ -9,7 +10,7 @@ import { OPCOES_DE_TIPO_DE_TELEFONE } from '../../models/dropdowns/opcoes-de-tip
 })
 export class PacienteTelefoneFormComponent {
 
-    @Input() 
+    @Input()
     public telefones: FormArray;
 
     public opcoesDeTipoDeTelefone = OPCOES_DE_TIPO_DE_TELEFONE;
@@ -37,6 +38,20 @@ export class PacienteTelefoneFormComponent {
             });
         }
         this.telefone.reset();
+    }
+
+    datatableClick(event: DatatableClickEvent) {
+        if (event.selection) {
+            switch (event.button) {
+                case "edit":
+                    console.log("Teste");
+                    break;
+                case "delete":
+                    console.log("Teste");
+                    this.telefones.clear();
+                    break;
+            }
+        }
     }
 
 }
