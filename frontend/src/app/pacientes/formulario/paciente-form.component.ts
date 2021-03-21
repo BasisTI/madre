f3dbb5de5a8ef2f7fbb3e8dc3bfd361fe5b90a58
francisco.aguiar <francisco.aguiar@basis.com.br>
2021-03-21 07:56:21 -0300

    Permitir que paciente seja incluido na lista, consertar validacoes posteriormente - BASIS-182735

--- frontend/src/app/pacientes/formulario/paciente-form.component.ts
+++ frontend/src/app/pacientes/formulario/paciente-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { CpfCnpjValidator } from '../../shared/cpf-cnpj.validator';
@@ -10,14 +10,94 @@ import { PacienteValidators } from "./paciente.validators";
     selector: 'paciente-form',
     templateUrl: './paciente-form.component.html',
 })
-export class PacienteFormComponent {
+export class PacienteFormComponent  implements OnInit{
 
-    constructor(private fb: FormBuilder, public pacienteService: PacienteService) {
+    paciente: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+    }
+    ngOnInit(): void {
+        this.paciente = this.fb.group({
+            id: [null],
+            nome: [null, Validators.required],
+            nomeSocial: [null],
+            dataDeNascimento: [null, Validators.required],
+            horaDeNascimento: [null],
+            email: [null, Validators.email],
+            observacao: [null],
+            ocupacaoId: [null],
+            religiaoId: [null],
+            etniaId: [null],
+            uf: [null],
+            naturalidadeId: [null],
+            nacionalidadeId: [null, Validators.required],
+            racaId: [null, Validators.required],
+            estadoCivilId: [null, Validators.required],
+            grauDeInstrucao: [null, Validators.required],
+            sexo: [null, Validators.required],
+            telefones: this.fb.array([]),
+            enderecos: this.fb.array([]),
+            genitores: this.fb.group({
+                id: [null],
+                prontuarioDaMae: [null],
+                nomeDaMae: [null, Validators.required],
+                nomeDoPai: [null, Validators.required],
+            }),
+            cartaoSUS: this.fb.group({
+                id: [null],
+                numero: ['',[Validators.required, PacienteValidators.validarNumero]],
+                documentoDeReferencia: [null],
+                cartaoNacionalSaudeMae: ['', PacienteValidators.validarNumero],
+                dataDeEntradaNoBrasil: [null],
+                dataDeNaturalizacao: [null],
+                portaria: [null],
+                justificativaId: [null],
+                motivoDoCadastroId: [null],
+            }),
+            responsavel: this.fb.group(
+                {
+                    id: [null],
+                    nomeDoResponsavel: [null],
+                    telefones: this.fb.array([]),
+                    observacao: [null],
+                    grauDeParentescoId: [null],
+                },
+                { updateOn: 'blur'},
+            ),
+            documento: this.fb.group(
+                {
+                    id: [null],
+                    numeroDaIdentidade: [null],
+                    data: [null],
+                    cpf: [null],
+                    pisPasep: [null],
+                    cnh: [null],
+                    validadeDaCnh: [null,],
+                    orgaoEmissorId: [null],
+                    ufId: [null],
+                },
+                { updateOn: 'blur'},
+            ),
+            certidao: this.fb.group({
+                id: [null],
+                registroDeNascimento: [null],
+                tipoDaCertidao: [null],
+                nomeDoCartorio: [null],
+                livro: [null],
+                folhas: [null],
+                termo: [null],
+                dataDeEmissao: [null],
+                numeroDaDeclaracaoDeNascimento: [null]
+            })
+        });
     }
 
+
+
+/*
     paciente: FormGroup = this.fb.group({
         id: [null],
-        nome: [null, Validators.required],
+        nome: [null, , Validators.required],
         nomeSocial: [null],
         dataDeNascimento: [null, Validators.required],
         horaDeNascimento: [null],
@@ -90,8 +170,6 @@ export class PacienteFormComponent {
 
     });
 
-    public savePaciente(paciente){
-        console.log("Apertou botão salvar");
-    }
+*/
 
 }
