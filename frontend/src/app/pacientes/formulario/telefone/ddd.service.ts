a8ff0100e44bc94df10c97d7fe99045694af1b92
francisco.aguiar <francisco.aguiar@basis.com.br>
2021-04-08 14:31:55 -0300

    Resolver problemas do sonar frontend - BASIS-189182

---
+++ frontend/src/app/pacientes/formulario/telefone/ddd.service.ts
@@ -0,0 +1,21 @@
+import { CrudServiceNuvem } from '@nuvem/primeng-components';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { DDD } from '../../models/dropdowns/types/DDD';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DDDService extends CrudServiceNuvem<number, DDD>{
+
+  uriServico: string = '/pacientes/api/list/ddd';
+
+  constructor(private httpClient: HttpClient) {
+    super('/pacientes/api/list/ddd', httpClient);
+  }
+
+/*getListaDeDDDs(): Observable<DDD[]>{
+    return this.httpClient.get<DDD[]>(this.uriServico);
+  }*/
+}
