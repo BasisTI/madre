2bf62a0a18ee191c24e44c65701a05b4384de4de
carlos.pereira <carlos.pereira@basis.com.br>
2021-04-29 11:03:40 -0300

    Adicionar filtro de  pesquisa consulta-emergencia - BASIS-182764

--- consulta/src/main/java/br/com/basis/consulta/service/EmergenciaService.java
+++ consulta/src/main/java/br/com/basis/consulta/service/EmergenciaService.java
@@ -6,15 +6,21 @@ import br.com.basis.consulta.repository.search.EmergenciaSearchRepository;
 import br.com.basis.consulta.service.dto.EmergenciaDTO;
 import br.com.basis.consulta.service.mapper.EmergenciaMapper;
 import br.com.basis.consulta.service.projection.CalendarioResumo;
+import org.elasticsearch.common.Strings;
+
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
-
 import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
 
 /**
@@ -31,6 +37,9 @@ public class EmergenciaService {
     private final EmergenciaMapper emergenciaMapper;
 
     private final EmergenciaSearchRepository emergenciaSearchRepository;
+
+    @Value("${application.elasticSearchFuzzyParameter}")
+    private static Integer elasticSearchFuzzyParam;
 
     public EmergenciaService(EmergenciaRepository emergenciaRepository, EmergenciaMapper emergenciaMapper, EmergenciaSearchRepository emergenciaSearchRepository) {
         this.emergenciaRepository = emergenciaRepository;
@@ -94,7 +103,7 @@ public class EmergenciaService {
     /**
      * Search for the emergencia corresponding to the query.
      *
-     * @param query the query of the search.
+     * @param query    the query of the search.
      * @param pageable the pagination information.
      * @return the list of entities.
      */
@@ -108,4 +117,64 @@ public class EmergenciaService {
     public Page<CalendarioResumo> buscarCalendarioResumo(Pageable pageable) {
         return emergenciaRepository.findAllCalendarioResumoBy(pageable);
     }
+
+    @Transactional(readOnly = true)
+    public Page<Emergencia> filtraConsultaEmergencial(Pageable pageable, String numeroConsulta, String grade, String pacienteId,String profissional, String especialidade, String clinicaCentral, String dataConsulta) {
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        filtraGrade(queryBuilder, grade);
+        filtraConsulta(queryBuilder, numeroConsulta);
+        filtraPaciente(queryBuilder, pacienteId);
+        filtraEspecialidade(queryBuilder,especialidade);
+        filtraProfissional(queryBuilder,profissional);
+        filtraClinicaCentral(queryBuilder,clinicaCentral);
+        filtraDataConsulta(queryBuilder,dataConsulta);
+        SearchQuery query = new NativeSearchQueryBuilder()
+            .withQuery(queryBuilder)
+            .withPageable(pageable)
+            .build();
+        return emergenciaSearchRepository.search(query);
+    }
+
+    private void filtraGrade(BoolQueryBuilder queryBuilder, String grade) {
+        if (!Strings.isNullOrEmpty(grade)) {
+            queryBuilder.must(QueryBuilders.matchQuery("grade", grade));
+        }
+    }
+
+    private void filtraConsulta(BoolQueryBuilder queryBuilder, String numeroConsulta) {
+        if (!Strings.isNullOrEmpty(numeroConsulta)) {
+            queryBuilder.must(QueryBuilders.matchQuery("numeroConsulta", numeroConsulta));
+        }
+    }
+
+    private void filtraPaciente(BoolQueryBuilder queryBuilder, String pacienteId) {
+        if (!Strings.isNullOrEmpty(pacienteId)) {
+            queryBuilder.must(QueryBuilders.matchQuery("pacienteId", pacienteId));
+        }
+    }
+
+    private void filtraProfissional(BoolQueryBuilder queryBuilder, String profissional){
+        if (!Strings.isNullOrEmpty(profissional)){
+            queryBuilder.must(QueryBuilders.matchQuery("profissional", profissional));
+        }
+    }
+
+    private void filtraEspecialidade(BoolQueryBuilder queryBuilder, String especialidade){
+        if (!Strings.isNullOrEmpty(especialidade)){
+            queryBuilder.must(QueryBuilders.matchQuery("especialidade", especialidade));
+        }
+    }
+
+    private void filtraClinicaCentral(BoolQueryBuilder queryBuilder, String clinicaCentral){
+        if (!Strings.isNullOrEmpty(clinicaCentral)){
+            queryBuilder.must(QueryBuilders.matchQuery("clinicaCentralId", clinicaCentral));
+        }
+    }
+
+    private void filtraDataConsulta(BoolQueryBuilder queryBuilder, String dataConsulta){
+        if (!Strings.isNullOrEmpty(dataConsulta)){
+            queryBuilder.must(QueryBuilders.matchQuery("dataHoraDaConsulta", dataConsulta));
+        }
+    }
 }
+
--- frontend/src/app/consulta/components/listar-consultas/listar-consultas.component.ts
+++ frontend/src/app/consulta/components/listar-consultas/listar-consultas.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { BreadcrumbService, CALENDAR_LOCALE } from '@nuvem/primeng-components';
+import { CRM } from '@internacao/models/crm';
+import { Especialidade } from '@internacao/models/especialidade';
+import { BreadcrumbService, CALENDAR_LOCALE, DatatableComponent } from '@nuvem/primeng-components';
 import { ConsultaEmergencia } from '../../consulta-emergencia-model';
 import { ConsultaService } from '../../consulta.service';
+import { ConsultaEmergenciaModel } from '../../models/consulta-emergencia-model';
+import { PacienteModel } from '../../models/paciente-model';
 
 @Component({
     selector: 'app-listar-consultas',
@@ -10,9 +14,28 @@ import { ConsultaService } from '../../consulta.service';
 })
 export class ListarConsultasComponent implements OnInit, OnDestroy {
 
-    searchUrl = 'madreconsulta/api/consultas-emergencias';
+    public crm = new Array<CRM>();
 
-    consulta: ConsultaEmergencia[];
+    public especialidades = new Array<Especialidade>();
+
+    public pacientes = new Array<PacienteModel>();
+
+    grade: string = '';
+    numeroConsulta: string = '';
+    clinicaCentralId: string = '';
+    dataConsulta: string = '';
+    data: Date;
+    paciente: any = '';
+    especialidade: any = '';
+    profissional: any = '';
+    prontuario: any = '';
+    results = [];
+    consulta: ConsultaEmergenciaModel[];
+
+    @ViewChild('datatable')
+    public datatable: DatatableComponent;
+ 
+    consultas: ConsultaEmergencia[];
 
     listaConsultas = this.fb.group({
         prontuario: [''],
@@ -41,7 +64,21 @@ export class ListarConsultasComponent implements OnInit, OnDestroy {
     dataLimite = new Date();
     anosDisponiveis = `2010:${this.dataLimite.getFullYear()}`;
     formatoDeData = 'dd/mm/yy';
+
     constructor(private fb: FormBuilder, private breadcrumbService: BreadcrumbService, private consultaService: ConsultaService) {}
+
+    formatarData(data: Date): string {
+        const normalize = (x: number): string => (x < 10 ? `0${x}` : `${x}`);
+
+        const dateObjectToFormattedString = (dateObject: Date): string => {
+            const year = normalize(dateObject.getFullYear());
+            const day = normalize(dateObject.getDate());
+            const month = normalize(dateObject.getMonth() + 1);
+
+            return `${year}-${month}-${day}`;
+        };
+        return dateObjectToFormattedString(data);
+    }
 
     ngOnInit(): void {
         this.breadcrumbService.setItems([
@@ -54,7 +91,80 @@ export class ListarConsultasComponent implements OnInit, OnDestroy {
                 routerLink: 'listar-consultas',
             },
         ]);
+        this.listar();
+        this.listarEspecialidade();
+        this.listarPacientes();
+        this.listaProfissional();
     }
+
+    listar() {
+        if (this.data != null) {
+            this.dataConsulta = this.formatarData(this.data);
+        }
+        this.consultaService
+            .getConsulta(this.grade, this.numeroConsulta, this.prontuario, this.clinicaCentralId, this.dataConsulta,this.especialidade, this.profissional, this.paciente)
+            .subscribe((response) => {
+              this.consulta = response;  
+            });
+    } 
+    
+    public limparPesquisa() {
+        this.listaConsultas.reset()
+        this.grade = "";
+        this.numeroConsulta = "";
+        this.prontuario = "";
+        this.clinicaCentralId = "";
+        this.data = null;
+        this.dataConsulta = "";
+        this.especialidade ="";
+        this.profissional ="";
+        this.paciente = "";
+        this.listar();
+    }
+
+    listarPacientes() {
+        this.consultaService.buscarPaciente().subscribe((pacientes: Array<PacienteModel>) => {
+            this.pacientes = pacientes;
+        });
+    }
+
+    listaProfissional(evento?) {
+        this.consultaService.buscarProfissionais().subscribe((crms: Array<CRM>) => {
+            this.crm = crms;
+        });
+    }
+
+    listarEspecialidade(){
+        this.consultaService.buscarEspecialidades().subscribe((especialidades: Array<Especialidade>)=> {
+            this.especialidades = especialidades;
+        })
+    }
+
+    selecionarProfissional(event){
+        this.crm.forEach(element => {
+            this.profissional = element.nome
+        });
+    }
+
+    selecionarEspecialidade(event){
+        this.especialidade = (<HTMLElement>event.target.value);
+         this.especialidades.forEach(element =>{
+             if (element.especialidade === this.especialidade) {
+                 this.especialidade = element.especialidade
+             }
+         });
+
+     }
+
+    selecionarPaciente(event){
+        this.paciente = (<HTMLElement>event.target.value);
+         this.pacientes.forEach(element =>{
+             if (element.nome === this.paciente) {
+                 this.paciente = element.id
+                 this.prontuario = element.prontuario;
+             }
+         });
+     }
 
     ngOnDestroy(): void {
         this.breadcrumbService.reset();
