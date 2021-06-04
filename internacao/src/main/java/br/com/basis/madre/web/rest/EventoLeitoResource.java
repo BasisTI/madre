75d9e2d2ea437d27fe637f1f90e641bb4e99ec20
Alexandre Felipe <73399723+alexandreafa@users.noreply.github.com>
2021-06-04 15:25:19 -0300

    Adicionar para apagar leito de evento_leito - BASIS-161698 (#278)
    * Funciona, mas dá erro 500
    
    * Adicionar para apagar leitos liberados de evento_leito - BASIS-161698
    
    Co-authored-by: Alexandre <alexandre.araujo@basis.com.br>

--- internacao/src/main/java/br/com/basis/madre/web/rest/EventoLeitoResource.java
+++ internacao/src/main/java/br/com/basis/madre/web/rest/EventoLeitoResource.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
