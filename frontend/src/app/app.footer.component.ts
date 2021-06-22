c6f98e310696f9cb623c3d5961d190ef0e2c5d8d
brenda <brenda.oliveira@basis.com.br>
2021-06-22 10:55:20 -0300

    app.footer-ok

--- frontend/src/app/app.footer.component.ts
+++ frontend/src/app/app.footer.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-footer',
     template: `
-        <div class="layout-footer p-d-flex p-ai-center p-p-4 p-shadow-2">
-            <span class="footer-text-left" styles="background: #C3CBD9;">MADRE- 2021. Todos os direitos reservados. - Versão 01.007.008-009</span>
+        <div class="layout-footer p-ai-center p-p-4 p-shadow-2">
+            <span class="footer-text-left" styles="background: #C3CBD9; padding: 20%;">MADRE- 2021. Todos os direitos reservados. - Versão 01.007.008-009</span>
     `,
 })
 export class AppFooterComponent {}
