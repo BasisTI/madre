79d0409d77f1bcfd78d24587dfa94621790e7de4
brenda <brenda.oliveira@basis.com.br>
2021-06-29 10:32:17 -0300

    tabelas -acoes/formulario-triagem - BASIS-204458

--- frontend/src/app/pacientes/formulario/paciente-list.component.ts
+++ frontend/src/app/pacientes/formulario/paciente-list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { DatatableParams } from "@nuvem/primeng-components";
+import { ElasticQuery } from '@shared/elastic-query';
+import { DatatableComponent, DatatableClickEvent, PageNotificationService } from '@nuvem/primeng-components';
 
 @Component({
     selector: 'paciente-list',
     templateUrl: './paciente-list.component.html',
 })
 export class PacienteListComponent {
+
+    elasticQuery: ElasticQuery = new ElasticQuery();
+    
+    @ViewChild(DatatableComponent) datatable: DatatableComponent;
 
     paciente: FormGroup = this.fb.group({
         nome: [],
@@ -18,6 +24,10 @@ export class PacienteListComponent {
     };
 
     constructor(private fb: FormBuilder) {
+    }
+
+    pesquisar(){
+        this.datatable.refresh(this.elasticQuery.query);
     }
 
 }
