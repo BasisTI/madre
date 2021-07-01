e0ec9d03a12ee8ee6534f348d0b863abb4ddc3ba
bomtempo <santosbomtempo@gmail.com>
2021-07-01 09:31:57 -0300

    modificaçao 01/07/21 BASIS-204456

--- frontend/src/app/app.footer.component.ts
+++ frontend/src/app/app.footer.component.ts
@@ -3,14 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-footer',
     template: `
-        <div class="layout-footer p-d-flex p-ai-center p-p-4 p-shadow-2">
-            <span class="footer-text-left">BASIS Tecnologia - </span>
-            <app-version-tag cssClass="footer-text-left"></app-version-tag>
-            <span class="p-ml-auto p-mr-2">
-                <span class="p-icon p-icon-copyright"></span>
-                <span>Todos os direitos reservados</span>
-            </span>
-        </div>
+        <div class="layout-footer p-ai-center p-p-4 p-shadow-2">
+            <span class="footer-text-left" styles="background:#C3CBD9 padding: 20%;">MADRE- 2021. Todos os direitos reservados. - <app-version-tag></app-version-tag></span>
     `,
 })
 export class AppFooterComponent {}
