146e1ae2f7c49d8f4a322a066c57bb78aec81d7c
brenda maria <mariabrenda177@gmail.com>
2021-07-01 09:24:40 -0300

    madre2.0 BASIS-204458

--- frontend/src/app/app.footer.component.ts
+++ frontend/src/app/app.footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'app-footer',
     template: `
         <div class="layout-footer p-ai-center p-p-4 p-shadow-2">
-            <span class="footer-text-left" styles="background: #C3CBD9; padding: 20%;">MADRE- 2021. Todos os direitos reservados. - Versão 01.007.008-009</span>
+            <span class="footer-text-left" styles="background: #C3CBD9; padding: 20%;">MADRE- 2021. Todos os direitos reservados. - <app-version-tag></app-version-tag></span>
     `,
 })
 export class AppFooterComponent {}
