92787cbb7946828a216de19efea91a171e17f89b
VitorZary <vitorhbts@gmail.com>
2021-07-13 16:48:39 -0300

    Botões do CRUD voltando a funcionar BASIS-208461

--- frontend/src/app/pacientes/formulario/paciente-list.component.ts
+++ frontend/src/app/pacientes/formulario/paciente-list.component.ts
@@ -4,33 +4,32 @@ import { DatatableParams } from "@nuvem/primeng-components";
 import { ElasticQuery } from '@shared/elastic-query';
 import { DatatableComponent, DatatableClickEvent, PageNotificationService } from '@nuvem/primeng-components';
 import { AfterContentInit } from "@angular/core";
+import { ConfirmationService } from "primeng/api";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PacienteService } from "./paciente.service";
 
 @Component({
     selector: 'paciente-list',
     templateUrl: './paciente-list.component.html',
 })
-export class PacienteListComponent implements AfterContentInit {
-
-
+export class PacienteListComponent {
 
     elasticQuery: ElasticQuery = new ElasticQuery();
     
     @ViewChild(DatatableComponent) datatable: DatatableComponent;
 
     paciente: FormGroup = this.fb.group({
-        nome: [],
+        nome: [''],
         prontuario: []
     });
 
-    datatableParams: DatatableParams = {
-        rows: 10
-    };
-
-    constructor(private fb: FormBuilder) {
-    }
-    ngAfterContentInit(): void {
-        throw new Error("Method not implemented.");
-    }
+    constructor(
+        private fb: FormBuilder,
+        private confirmationService: ConfirmationService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: PacienteService
+    ) { }
 
     searchUrl:string = 'pacientes/api/_search/pacientes';
 
@@ -44,5 +43,22 @@ export class PacienteListComponent implements AfterContentInit {
         this.pesquisar();
     }
     
+    datatableClick(event: DatatableClickEvent) {
+        if (event.selection) {
+            switch (event.button) {
+                case "edit":
+                case "view":
+                    this.router.navigate([`../${event.button}`, event.selection.id], { relativeTo: this.route });
+                    break;
+                case "delete":
+                    this.confirmationService.confirm({
+                        message: 'Você tem certeza que deseja excluir o registro?',
+                        accept: () => this.service.delete(event.selection.id)
+                            .subscribe(() => this.pesquisar()),
+                    });
+                    break;
+            }
+        }
+    }
 
 }
