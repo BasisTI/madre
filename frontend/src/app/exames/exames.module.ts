059a94aa8a3302ecf0a5ead96bdcfa42bd133a5b
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-07-15 11:55:30 -0300

    Corrigir erro no deploy do menu Exames - BASIS-206711

--- frontend/src/app/exames/exames.module.ts
+++ frontend/src/app/exames/exames.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PesquisarAtendimentoComponent } from './components/pesquisar-atendimento/pesquisar-atendimento.component';
 import { SolicitarExameComponent } from './components/solicitar-exame/solicitar-exame.component';
 import { AtendimentoDiversoComponent } from "./components/atendimento-diverso/atendimento-diverso.component";
-import { ExamesComponent } from "./components/exames/exames.component";
 import { SharedModule } from '@shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { routes } from './exames.routes';
+import { ExamesComponent } from './components/exames/exames.component';
 
 
 
@@ -14,9 +14,8 @@ import { routes } from './exames.routes';
     declarations: [
         PesquisarAtendimentoComponent,
         SolicitarExameComponent,
-        ExamesComponent,
-        AtendimentoDiversoComponent
-
+        AtendimentoDiversoComponent,
+        ExamesComponent
     ],
     imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
 })
