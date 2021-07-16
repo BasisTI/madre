40f5965ecf1976e411a0a2d37b26f7b918a25d65
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-07-16 12:30:46 -0300

    Adicionar opçoes de dropdown - BASIS-206711

--- frontend/src/app/exames/components/pesquisar-atendimento/pesquisar-atendimento.component.ts
+++ frontend/src/app/exames/components/pesquisar-atendimento/pesquisar-atendimento.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OPCOES_DE_ORIGEM } from '../../models/dropdowns/opcoes-de-origem';
+import { UnidadeFuncional } from '../../models/subjects/unidade-funcional-model';
+import { UnidadeFuncionalService } from '../../services/unidade-funcional.service';
 
 @Component({
   selector: 'app-pesquisar-atendimento',
@@ -8,12 +10,18 @@ import { OPCOES_DE_ORIGEM } from '../../models/dropdowns/opcoes-de-origem';
 })
 export class PesquisarAtendimentoComponent implements OnInit {
 
-  constructor() { }
-
   opcoesDeOrigem = OPCOES_DE_ORIGEM;
+  unidadesFuncionais: UnidadeFuncional[] = [];
+
+  constructor(private unidadeFuncionalService: UnidadeFuncionalService) { }
+
 
 
   ngOnInit(): void {
+    this.unidadeFuncionalService.getUnidadeFuncional().subscribe((response) => {
+      this.unidadesFuncionais = response;
+
+    });
   }
 
 }
