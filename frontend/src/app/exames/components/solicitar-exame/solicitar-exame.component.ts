5fdfdc251c33fb71b04201d6556420f8713daad5
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-07-19 14:25:08 -0300

    Adicionar opçao responsavel em Solicitar Exame - BASIS-206711

--- frontend/src/app/exames/components/solicitar-exame/solicitar-exame.component.ts
+++ frontend/src/app/exames/components/solicitar-exame/solicitar-exame.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Responsavel } from '../../models/subjects/responsavel-model';
+import { UnidadeFuncional } from '../../models/subjects/unidade-model';
+import { ResponsavelService } from '../../services/responsavel.service';
+import { UnidadeFuncionalService } from '../../services/unidade-funcional.service';
 
 @Component({
   selector: 'app-solicitar-exame',
@@ -7,12 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitarExameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private unidadeFuncionalService: UnidadeFuncionalService,
+              private responsavelService: ResponsavelService) { }
 
   opcoesDeAntimicrobianos: string;
   opcoesDeExameOuComparativo: string;
+  unidades: UnidadeFuncional[] = [];
+  responsaveis: Responsavel[] = [];
+
 
   ngOnInit(): void {
+    this.unidadeFuncionalService.GetUnidades().subscribe((response) => {
+      this.unidades = response;
+    });
+
+    this.responsavelService.getResponsavel().subscribe((response) => {
+      this.responsaveis = response;
+    });
   }
 
 }
