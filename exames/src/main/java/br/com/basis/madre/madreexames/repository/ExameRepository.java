78412f12299274abe3fd0af7762ae52e63f6c2e6
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-07-21 11:48:56 -0300

    Adicionar endpoint para relacionamento de Exames e grupo - BASIS-206709

--- exames/src/main/java/br/com/basis/madre/madreexames/repository/ExameRepository.java
+++ exames/src/main/java/br/com/basis/madre/madreexames/repository/ExameRepository.java
@@ -2,8 +2,11 @@ package br.com.basis.madre.madreexames.repository;
 
 import br.com.basis.madre.madreexames.domain.Exame;
 
+import br.com.basis.madre.madreexames.service.dto.ExameDTO;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Spring Data  repository for the Exame entity.
@@ -11,4 +14,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface ExameRepository extends JpaRepository<Exame, Long> {
+
+    List<Exame> findByGrupoAgendamentoExamesId(Long id);
 }
