7d1c44f35941b012ca99b180ec88f9018108712b
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-07-26 08:40:03 -0300

    Corrigir dropdown de origens - BASIS-206709

--- frontend/src/app/exames/components/pesquisar-atendimento/pesquisar-atendimento.component.ts
+++ frontend/src/app/exames/components/pesquisar-atendimento/pesquisar-atendimento.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { OrigemDaInternacao } from '@internacao/models/origem-da-internacao';
 import { OrigemDaInternacaoService } from '@internacao/services/origem-da-internacao.service';
 import { UnidadeFuncional } from '../../models/subjects/unidade-model';
-import { OPCOES_DE_ORIGEM } from '../../models/dropdowns/opcoes-de-origem';
 import { UnidadeFuncionalService } from '../../services/unidade-funcional.service';
 
 @Component({
