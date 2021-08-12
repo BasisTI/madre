7ef4084f9d7bae1faa8eb611d95f3d44376be086
brenda maria <mariabrenda177@gmail.com>
2021-08-12 09:02:46 -0300

    adicionar rotas e componente para ocupacoes de cargo - BASIS-220351

---
+++ frontend/src/app/seguranca/components/ocupacoes-de-cargo/ocupacoes-de-cargo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ocupacoes-de-cargo',
+  templateUrl: './ocupacoes-de-cargo.component.html',
+  styleUrls: ['./ocupacoes-de-cargo.component.css']
+})
+export class OcupacoesDeCargoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/seguranca/seguranca.routes.ts
+++ frontend/src/app/seguranca/seguranca.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { OcupacoesDeCargoComponent } from './components/ocupacoes-de-cargo/ocupacoes-de-cargo.component';
 import { FormularioServidorComponent } from './components/servidores/formulario-servidor/formulario-servidor.component';
 import { ServidoresComponent } from './components/servidores/servidores.component';
 import { VinculosComponent } from './components/vinculos/vinculos.component';
@@ -7,5 +8,6 @@ export const routes: Routes = [
     { path: 'servidores', component: ServidoresComponent },
     { path: 'formulario', component: FormularioServidorComponent },
     { path: 'vinculos', component: VinculosComponent },
+    { path: 'ocupacoes-de-cargo', component: OcupacoesDeCargoComponent },
 
 ];
