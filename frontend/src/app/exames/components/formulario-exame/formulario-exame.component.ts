0d1f23a5c1fb67e1f5704d10d51f73906d916814
yukioz <yukio.link@gmail.com>
2021-08-20 14:41:38 -0300

    criar formGroup de material de Exame. BASIS-221543
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- frontend/src/app/exames/components/formulario-exame/formulario-exame.component.ts
+++ frontend/src/app/exames/components/formulario-exame/formulario-exame.component.ts
@@ -5,7 +5,10 @@ import { Amostra } from '../../models/subjects/amostra';
 import { ExamModel } from '../../models/subjects/exames-model';
 import { Material } from '../../models/subjects/material';
 import { ExamesService } from '../../services/exames.service';
-
+import { atividadeDropdown } from '../../models/dropdowns/atividade.dropdown';
+import { naturezaDropdown } from '../../models/dropdowns/natureza.dropdown';
+import { sumarioDropdown } from '../../models/dropdowns/sumario.dropdown';
+import { unidadeTempoDropdown } from '../../models/dropdowns/unidadeDeTempo.dropdown'
 @Component({
   selector: 'app-formulario-exame',
   templateUrl: './formulario-exame.component.html',
@@ -14,7 +17,7 @@ import { ExamesService } from '../../services/exames.service';
 export class FormularioExameComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
-              private exameService: ExamesService) { }
+    private exameService: ExamesService) { }
 
   materiais: Material[] = [];
   amostras: Amostra[] = [];
@@ -36,19 +39,70 @@ export class FormularioExameComponent implements OnInit {
     anexaDocumentos: [null, Validators.required]
   });
 
+  // Material
+  cadastrarMaterial = this.fb.group({
+    nome: [null, Validators.required],
+    ativo: [null, Validators.required],
+    npo: [null, Validators.required],
+    jejum: [null, Validators.required],
+    exigePreparo: [null, Validators.required],
+    exigeDieta: [null, Validators.required],
+    informaNumeroDeColetas: [null, Validators.required],
+    geraItemDeSolicitacao: [null, Validators.required],
+    exigeIntervaloDeColeta: [null, Validators.required],
+    exigeRegiaoAnatomica: [null, Validators.required],
+    ingestaoDeMedicamento: [null, Validators.required],
+    dependenteDeExame: [null, Validators.required],
+    analisadoPelaCII: [null, Validators.required], // faltou
+    interesseDaCOMEDI: [null, Validators.required],
+    exigeImpressao: [null, Validators.required],
+    apareceResultado: [null, Validators.required],
+    contaCelulas: [null, Validators.required],
+    limiteDeSolicitacao: [null, Validators.required],
+    formaDeRespiracao: [null, Validators.required],
+    automatico: [null, Validators.required],
+    exigeDadosComplementares: [null, Validators.required],
+    natureza: [null, Validators.required],
+    sumario: [null, Validators.required],
+    tempoJejum: [null, Validators.required], // aki
+    intervaloMinimo: [null, Validators.required],
+    unidadeDeTempo: [null, Validators.required],
+    validade: [null, Validators.required],
+    agendamentoMinimo: [null, Validators.required],
+    tempoLimiteDaSolicitacao: [null, Validators.required],
+    unidadeDeTempoDaSolicitacao: [null, Validators.required],
+    numeroDeAmostras: [null, Validators.required],
+    numeroDeAmostrasPadrao: [null, Validators.required],
+    diasLimiteDefault: [null, Validators.required],
+    tempoLimiteDefault: [null, Validators.required],
+    numeroDeAmostrarPorIntervalo: [null, Validators.required],
+    tempoLimiteDeAmostraPorIntervalo: [null, Validators.required],
+    unidadeLimiteDeTempoDoPeriodo: [null, Validators.required],
+    permiteSolicitacaoPosAlta: [null, Validators.required],
+    tempoPermitidoParaSolicitarPosAlta: [null, Validators.required],
+    tempoPermitidoParaSolicitarPosAltaPelasAreasExecutoras: [null, Validators.required],
+    cartaDeColeta: [null, Validators.required],
+    laboratoriaTercerizado: [null, Validators.required],
+    naoCancelaExamaAposAlta: [null, Validators.required],
+  })
+
+  atividade = atividadeDropdown;
+  natureza = naturezaDropdown;
+  sumario = sumarioDropdown;
+  unidadeDeTempoOptions = unidadeTempoDropdown;
 
   valid(): boolean {
-    if(this.cadastrarExame.valid && this.amostraSelecionada && this.materialSelecionado)
+    if (this.cadastrarExame.valid && this.amostraSelecionada && this.materialSelecionado)
       return true;
     else
-    return false;
+      return false;
   }
 
   limpar() {
     this.cadastrarExame.reset();
   }
 
-  cadastrar(){
+  cadastrar() {
     let cadastroExame = this.cadastrarExame.value;
 
     let cadastro: ExamModel = {
---
+++ frontend/src/app/exames/models/dropdowns/sumario.dropdown.ts
@@ -0,0 +1,21 @@
+import { OpcaoDropdown } from '@shared/dropdown/opcao.dropdown';
+
+
+export const sumarioDropdown: OpcaoDropdown[] = [
+    {
+        label: 'Bioquimica',
+        value: 'Bioquimica',
+    },
+    {
+        label: 'EQU',
+        value: 'EQU',
+    },
+    {
+        label: 'Gasometria',
+        value: 'Gasometria',
+    },
+    {
+        label: 'Hematologia',
+        value: 'Hematologia',
+    },
+];
