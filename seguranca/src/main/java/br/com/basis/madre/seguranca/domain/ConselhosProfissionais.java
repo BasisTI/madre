ecca14eced9c9d068dc4b8b5deda5c7e42cc196b
brenda maria <mariabrenda177@gmail.com>
2021-09-01 09:34:26 -0300

    import javax.persistence.Entity;

--- seguranca/src/main/java/br/com/basis/madre/seguranca/domain/ConselhosProfissionais.java
+++ seguranca/src/main/java/br/com/basis/madre/seguranca/domain/ConselhosProfissionais.java
@@ -15,7 +15,6 @@ import javax.persistence.Column;
 import javax.persistence.OneToMany;
 import javax.persistence.GenerationType;
 
-
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.HashSet;
