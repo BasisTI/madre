5c503a84a3b0289615ead0f159cedad44ff88c0c
brenda maria <mariabrenda177@gmail.com>
2021-09-01 09:48:45 -0300

    resolver code smell de TiposDeQualificacao.java - BASIS-221273

--- seguranca/src/main/java/br/com/basis/madre/seguranca/domain/TiposDeQualificacao.java
+++ seguranca/src/main/java/br/com/basis/madre/seguranca/domain/TiposDeQualificacao.java
@@ -3,8 +3,19 @@ package br.com.basis.madre.seguranca.domain;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.GenerationType;
+
+import javax.validation.constraints.NotNull;
 
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.io.Serializable;
