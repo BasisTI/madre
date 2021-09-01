3d974171e280d167e432b02a5071420bb14f137e
brenda maria <mariabrenda177@gmail.com>
2021-09-01 09:46:07 -0300

    resolver code smell de Instituicao.java - BASIS-221273

--- seguranca/src/main/java/br/com/basis/madre/seguranca/domain/Instituicao.java
+++ seguranca/src/main/java/br/com/basis/madre/seguranca/domain/Instituicao.java
@@ -4,8 +4,18 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.GenerationType;
+
+import javax.validation.constraints.NotNull;
 
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.io.Serializable;
