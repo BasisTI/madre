fa37c6b5c9db1b40cfbc77c5df6b982e5e015911
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-09-16 11:00:53 -0300

    Adicionar DTO de Servidor e UnidadeFuncional em Exames - BASIS-214044

--- exames/src/main/java/br/com/basis/madre/madreexames/service/integracao/InternacaoClient.java
+++ exames/src/main/java/br/com/basis/madre/madreexames/service/integracao/InternacaoClient.java
@@ -1,6 +1,6 @@
 package br.com.basis.madre.madreexames.service.integracao;
 
-import br.com.basis.madre.domain.UnidadeFuncional;
+import br.com.basis.madre.madreexames.service.dto.integracao.UnidadeFuncionalDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface InternacaoClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/unidades-funcionais/{id}")
-    UnidadeFuncional getUnidadeFuncional(@PathVariable("id") long id);
+    UnidadeFuncionalDTO getUnidadeFuncional(@PathVariable("id") long id);
 
 }
--- exames/src/main/java/br/com/basis/madre/madreexames/service/integracao/SegurancaClient.java
+++ exames/src/main/java/br/com/basis/madre/madreexames/service/integracao/SegurancaClient.java
@@ -1,6 +1,6 @@
 package br.com.basis.madre.madreexames.service.integracao;
 
-import br.com.basis.madre.seguranca.service.dto.ServidorDTO;
+import br.com.basis.madre.madreexames.service.dto.integracao.ServidorDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
